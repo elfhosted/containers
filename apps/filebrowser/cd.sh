aeed5d8481b90d2d80883d650061964f36553175
David Young <davidy@funkypenguin.co.nz>
2023-06-17 23:53:19 +1200

    Bump filebrowser
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/filebrowser/cd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "It's not possible to use [cd] to change directories in Filebrowser"
-echo "Instead, navigate the file structure using the GUI elements above, and the $PWD in the"
-echo "console will change as you do :)"
