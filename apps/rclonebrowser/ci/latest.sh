bc996b653d3cc01f88285bb5c8282fd671642842
David Young <davidy@funkypenguin.co.nz>
2023-06-27 10:48:03 +1200

    First attempt
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rclonebrowser/ci/latest.sh
+++ apps/rclonebrowser/ci/latest.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/crazy-max/docker-rtorrent-rutorrent/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version=$(curl -sX GET https://api.github.com/repos/rclone/rclone/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 version="${version#*v}"
 version="${version#*release-}"
-version="${version%-r0}"
-version="${version%-r1}"
-# printf "%s" "${version}"
-echo develop # temporary
+printf "%s" "${version}"
