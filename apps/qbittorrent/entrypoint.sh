6e973b64dbadeed1495dc7a4c4d9984874198929
David Young <davidy@funkypenguin.co.nz>
2023-07-02 16:41:18 +1200

    Update qbit to delay starting while waiting for forwarded port to appear
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/qbittorrent/entrypoint.sh
+++ apps/qbittorrent/entrypoint.sh
@@ -16,10 +16,10 @@ fi
 mkdir -p /config/qBittorrent/logs/
 ln -sf /proc/self/fd/1 "$qbtLogFile"
 
-
 # If we have been given a port by pia/gluetun, insert it into the config before starting
 # Apply the port
 if [[ ! -z "$PORT_FILE" ]]; then
+    sleep 10s # time for VPN to populate this file
     sed -i  "s/Connection\\\PortRangeMin=.*/Connection\\\PortRangeMin=$(cat $PORT_FILE)/" /config/qBittorrent/qBittorrent.conf
     sed -i  "s/Session\\\Port=.*/Session\\\Port=$(cat $PORT_FILE)/" /config/qBittorrent/qBittorrent.conf
 fi 
