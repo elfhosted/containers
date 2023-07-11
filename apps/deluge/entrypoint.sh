c5fb45553eba545fd138e79a184446d764d14f69
David Young <davidy@funkypenguin.co.nz>
2023-07-12 10:11:33 +1200

    Make deluge work with pia
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/deluge/entrypoint.sh
+++ apps/deluge/entrypoint.sh
@@ -2,12 +2,21 @@
 
 set -e
 
-qbtLogFile=/config/qBittorrent/qBittorrent/logs/qbittorrent.log
-if [ -f "$qbtLogFile" ] 
-then
-    rm $qbtLogFile
-fi
-mkdir -p /config/qBittorrent/qBittorrent/logs/
-ln -sf /proc/self/fd/1 "$qbtLogFile"
+#shellcheck disable=SC1091
+source "/scripts/umask.sh"
+source "/scripts/vpn.sh"
 
-exec /usr/local/bin/qbittorrent-nox
+# If we have been given a port by pia/gluetun, insert it into the config before starting
+if [[ ! -z "$PORT_FILE" ]]; then
+    # Wait for port file to appear
+    until [[ -f $PORT_FILE ]]; do
+        echo waiting for port-forward details...
+        sleep 5s
+    done
+        JQ_FILTER=" .listen_ports=[$(cat $PORT_FILE),$(cat $PORT_FILE)]"
+        jq "${JQ_FILTER}" /config/core.conf > /config/core-new.conf
+        cp /config/core-new.conf /config/core.conf
+fi 
+
+# Assume wireguard
+exec /usr/bin/deluged -L info -d -c /config -o ${VPN_IF:=wg0}
