1d0a1c398e99d44c9dd6ddac7c4100b18a88ca6e
David Young <davidy@funkypenguin.co.nz>
2023-08-23 16:47:34 +1200

    Build custom lightweight rclone image
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/rclone/ci/latest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=rclone&inrepo=alpine_edge" | jq -r '.rclone | .[] | select((.repo == "alpine_edge" and .binname == "rclone")) | .version')
+version="${version%%_*}"
+version="${version%%-*}"
+printf "%s" "${version}"
