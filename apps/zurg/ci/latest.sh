8b9795e503208832b73c9cd9dabd7292909f0369
David Young <davidy@funkypenguin.co.nz>
2023-12-26 12:00:48 +1300

    hard-code zurg version for now
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/zurg/ci/latest.sh
+++ apps/zurg/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg-testing/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-# version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+# version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg-testing/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+# version="${version#*release-}"
+# printf "%s" "${version}"
+printf "%s" "v0.9.2-hotfix.4"
