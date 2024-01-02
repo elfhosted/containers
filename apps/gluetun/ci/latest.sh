9a728acfcec1d64ddf800c596e94763af04b980f
David Young <davidy@funkypenguin.co.nz>
2024-01-02 15:12:13 +1300

    Try to make VPN_ENDPOINT_IP work with DNS
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/gluetun/ci/latest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET https://api.github.com/repos/qdm12/gluetun/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version="${version#*v}"
+printf "%s" "${version}"
---
+++ apps/gluetun/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# If the VPN_ENDPOINT_IP is not an IP address, then convert it to one
+
+if echo "$VPN_ENDPOINT_IP" | egrep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'
+then
+    echo "No changes, VPN_ENDPOINT_IP is an IP"
+else
+    export VPN_ENDPOINT_IP=$(dig +short $VPN_ENDPOINT_IP)
+fi
+
+exec \
+    /gluetun-entrypoint
