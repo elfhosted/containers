55c5310cdcdb1bc038237be0c783d36f0cc44413
David Young <davidy@funkypenguin.co.nz>
2024-01-06 15:43:46 +1300

    Update versioning for plex-debrid
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plex-debrid/ci/latest.sh
+++ apps/plex-debrid/ci/latest.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-# version=$(curl -sX GET "https://api.github.com/repos/nzbgetcom/nzbget/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')
-# version="${version#*v}"
-# version="${version#*release-}"
-# printf "%s" "${version}"
-printf "%s" "latest"
+version=$(curl -sX GET "https://api.github.com/repos/itsToggle/plex_debrid/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+printf "%s" "dev-${version}"
