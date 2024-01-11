07ddab7de96c3d3f0f26c82fcaa84f17abe985f6
David Young <davidy@funkypenguin.co.nz>
2024-01-11 16:55:13 +1300

    Bump git-crypt to v0.7.0
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/concourse-github-pr-resource/scripts/install_git_crypt.sh
+++ apps/concourse-github-pr-resource/scripts/install_git_crypt.sh
@@ -9,9 +9,9 @@ _main() {
   tmpdir="$(mktemp -d git_crypt_install.XXXXXX)"
 
   cd "$tmpdir"
-  curl -Lo git-crypt-0.6.0.tar.gz https://www.agwa.name/projects/git-crypt/downloads/git-crypt-0.6.0.tar.gz
-  tar -zxf git-crypt-0.6.0.tar.gz
-  cd git-crypt-0.6.0
+  curl -Lo git-crypt-0.7.0.tar.gz https://www.agwa.name/projects/git-crypt/downloads/git-crypt-0.7.0.tar.gz
+  tar -zxf git-crypt-0.7.0.tar.gz
+  cd git-crypt-0.7.0
   make
   make install
   cd ..
