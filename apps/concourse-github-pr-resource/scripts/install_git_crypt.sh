99016c15394435bb02b553b621d674a69e9ce279
David Young <davidy@funkypenguin.co.nz>
2024-01-11 17:04:16 +1300

    try patching git-crypt
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/concourse-github-pr-resource/scripts/install_git_crypt.sh
+++ apps/concourse-github-pr-resource/scripts/install_git_crypt.sh
@@ -3,7 +3,7 @@
 set -eu
 
 _main() {
-  apk --update add --virtual=.build-deps curl make g++ openssl-dev
+  apk --update add --virtual=.build-deps curl make g++ openssl-dev git
   apk add --no-cache libgcc libstdc++ openssl
   local tmpdir
   tmpdir="$(mktemp -d git_crypt_install.XXXXXX)"
@@ -12,6 +12,7 @@ _main() {
   curl -Lo git-crypt-0.7.0.tar.gz https://www.agwa.name/projects/git-crypt/downloads/git-crypt-0.7.0.tar.gz
   tar -zxf git-crypt-0.7.0.tar.gz
   cd git-crypt-0.7.0
+  curl -L https://patch-diff.githubusercontent.com/raw/AGWA/git-crypt/pull/249.patch | git apply -v
   make
   make install
   cd ..
