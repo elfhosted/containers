260ae4ff4c54d9002c90c7fc6c0dfcd563345351
David Young <davidy@funkypenguin.co.nz>
2024-01-14 13:01:30 +1300

    Update rdtclient for laster13 variant
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rdtclient/ci/latest.sh
+++ apps/rdtclient/ci/latest.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/rogerfar/rdt-client/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+if [[ "${channel}" == "laster13" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/laster13/rdt-client/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/rogerfar/rdt-client/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
+
 printf "%s" "${version}"
