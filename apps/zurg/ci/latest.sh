8ec4d14896e603f664ce2b83a30ec9a9fd2df74b
David Young <davidy@funkypenguin.co.nz>
2024-01-24 14:36:44 +1300

    try hotfix.2
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/zurg/ci/latest.sh
+++ apps/zurg/ci/latest.sh
@@ -2,4 +2,4 @@
 # version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg-testing/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 # version="${version#*release-}"
 # printf "%s" "${version}"
-printf "%s" "v0.9.3-hotfix.1"
+printf "%s" "v0.9.3-hotfix.2"
