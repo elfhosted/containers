47e3aac3c88f18587cab4d4ded7d5e72de40cd80
David Young <davidy@funkypenguin.co.nz>
2024-01-25 12:07:59 +1300

    Another attempt
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/alpine/ci/latest.sh
+++ apps/alpine/ci/latest.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 channel=$1
-version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/alpine/tags?ordering=name&name=$channel" | jq --raw-output --arg s "$channel" '.results[] | select(.name | contains($s)) | .name'  | head -n1)
-version="${version#*v}"
-version="${version#*release-}"
+if [[ "${channel}" == "node-20" ]]; then
+    version="20"
+else
+    version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/alpine/tags?ordering=name&name=$channel" | jq --raw-output --arg s "$channel" '.results[] | select(.name | contains($s)) | .name'  | head -n1)
+    version="${version#*v}"
+    version="${version#*release-}"
+fi
 printf "%s" "${version}"
+
+
