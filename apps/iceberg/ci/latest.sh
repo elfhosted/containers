24360281dd95758b2e8266c21abc57b6cfb952a1
David Young <davidy@funkypenguin.co.nz>
2024-01-26 11:34:13 +1300

    build latest iceberg
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/iceberg/ci/latest.sh
+++ apps/iceberg/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET "https://api.github.com/repos/dreulavelle/iceberg/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-printf "%s" "dev-${version}"
+version=$(curl -sX GET "https://api.github.com/repos/dreulavelle/iceberg/commits/dev" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+printf "%s" "${version}"
