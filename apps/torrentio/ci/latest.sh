ecffc0793960772141a68194208caa48e4de843c
David Young <davidy@funkypenguin.co.nz>
2024-01-26 15:22:31 +1300

    Update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/torrentio/ci/latest.sh
+++ apps/torrentio/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET "https://api.github.com/repos/Pukabyte/torrentio-scraper-sh/commits/dev" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET "https://api.github.com/repos/Pukabyte/torrentio-scraper-sh/commits/master" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 printf "%s" "${version}"
