d21aebffc8a74012da544325463118f99cccdf3d
David Young <davidy@funkypenguin.co.nz>
2024-01-30 21:25:02 +1300

    Auto-backup Plex preferences
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plex/plex-preferences.sh
+++ apps/plex/plex-preferences.sh
@@ -38,15 +38,25 @@ if [ ! -e "${prefFile}" ]; then
 EOF
 fi
 
+# If preferences file is non-empty, then create a backup
+if [ -s "${prefFile}" ]; then
+  cp "${prefFile}" "${prefFile}-backup"
+fi
+
+# If the preferences file is empty, and we have a backup, then restore it
 if [ ! -s "${prefFile}" ]; then
-  echo "Creating pref shell"
+  if [ -s "${prefFile}-backup" ]; then
+    echo "${prefFile} was empty, restoring with copy from ${prefFile}-backup..."
+    cp "${prefFile}-backup" "${prefFile}"
+  else
+  echo "${prefFile} is empty and we have no backup, so creating a new one..."
   mkdir -p "$(dirname "${prefFile}")"
   cat > "${prefFile}" <<-EOF
 <?xml version="1.0" encoding="utf-8"?>
 <Preferences/>
 EOF
 fi
-
+fi
 
 # Setup Server's client identifier
 serial="$(getPref "MachineIdentifier")"
