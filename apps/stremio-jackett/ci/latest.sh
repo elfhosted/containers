a2e7a467a44218568cfd7f2ffc0ec1377db4be16
David Young <davidy@funkypenguin.co.nz>
2024-02-01 13:57:55 +1300

    Bump stremio-jackett to latest
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/stremio-jackett/ci/latest.sh
+++ apps/stremio-jackett/ci/latest.sh
@@ -2,6 +2,4 @@
 version=$(curl -sX GET https://api.github.com/repos/aymene69/stremio-jackett/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 # version=$(curl -sX GET "https://api.github.com/repos/aymene69/stremio-jackett/commits/dev" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 
-version="${version#*v}"
-version="${version#*release-}"
 printf "%s" "${version}"
