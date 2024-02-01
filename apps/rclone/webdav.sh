3b73843a3a428b7a62d546d4a50c5cec34af347a
David Young <davidy@funkypenguin.co.nz>
2024-02-01 21:52:29 +1300

    Another potential update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rclone/webdav.sh
+++ apps/rclone/webdav.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 /usr/bin/rclone serve webdav \
-    --config=/config/rclone.conf \
     --addr :5574 \
     --user ${WEBDAV_USERNAME} \
     --pass ${WEBDAV_PASSWORD} \
     --max-header-bytes=8192 \
-    --transfers=1 \
-    --multi-thread-streams=1 \
     --no-modtime \
     --vfs-read-chunk-size=10M \
     --vfs-read-chunk-size-limit=10M \    
