5361f8b33be1b84a9a1abc03e34d08095eabd6a6
David Young <davidy@funkypenguin.co.nz>
2024-02-10 08:58:00 +1300

    Try adding scannarr
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/scannarr/ci/latest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+version=$(curl -sX GET "https://api.github.com/repos/Pukabyte/scannarr/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+printf "%s" "${version}"
+
