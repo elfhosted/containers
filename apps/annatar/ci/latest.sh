0e1d93821f9f5afda96bdad5d5c7cbb3b45dc7ee
David Young <davidy@funkypenguin.co.nz>
2024-02-18 09:18:25 +1300

    Update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/annatar/ci/latest.sh
+++ apps/annatar/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-
-version=$(git ls-remote https://gitlab.com/stremio-add-ons/annatar.git HEAD | awk '{ print $1}')
+version=$(curl -SLs https://gitlab.com/api/v4/projects/54800933/releases | jq -r '.[0].tag_name')
+version="${version#*v}"
 printf "%s" "${version}"
