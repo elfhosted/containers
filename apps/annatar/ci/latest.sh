2b14cf78ecd371c7a8297826942f15e19d0415ae
David Young <davidy@funkypenguin.co.nz>
2024-02-18 09:20:50 +1300

    Update annatar versioning
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/annatar/ci/latest.sh
+++ apps/annatar/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 version=$(curl -SLs https://gitlab.com/api/v4/projects/54800933/releases | jq -r '.[0].tag_name')
-version="${version#*v}"
+# version="${version#*v}"
 printf "%s" "${version}"
