bf3e00a76ed35258ccffc0d49262ec12da4acfbc
David Young <davidy@funkypenguin.co.nz>
2024-02-20 13:42:54 +1300

    build off releases
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/mediafusion/ci/latest.sh
+++ apps/mediafusion/ci/latest.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-
-version=$(curl -sX GET "https://api.github.com/repos/mhdzumair/MediaFusion/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET https://api.github.com/repos/mhdzumair/MediaFusion/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version="${version#*v}"
+version="${version#*release-}"
 printf "%s" "${version}"
