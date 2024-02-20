aa8abe34ae5d1bf9b81abb4cd43ce8ae07c6cb9d
David Young <davidy@funkypenguin.co.nz>
2024-02-20 23:25:07 +1300

    Update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/stremio-jackett-community/ci/latest.sh
+++ apps/stremio-jackett-community/ci/latest.sh
@@ -1,3 +1,2 @@
 version=$(curl -sX GET https://api.github.com/repos/aymene69/stremio-jackett-community/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*v}"
 printf "%s" "${version}"
