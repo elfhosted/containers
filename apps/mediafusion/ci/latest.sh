88523276890fd0c48b8466314433c80b1d8a5360
David Young <davidy@funkypenguin.co.nz>
2024-02-20 23:19:21 +1300

    build stremio-jackett-community
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/mediafusion/ci/latest.sh
+++ apps/mediafusion/ci/latest.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET https://api.github.com/repos/mhdzumair/MediaFusion/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 version="${version#*v}"
-version="${version#*release-}"
 printf "%s" "${version}"
