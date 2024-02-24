9988a2496c081fc237872fbaa2a782ff9bc8e49f
David Young <davidy@funkypenguin.co.nz>
2024-02-24 23:04:17 +1300

    fix rdtclient build
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rdtclient/ci/latest.sh
+++ apps/rdtclient/ci/latest.sh
@@ -3,7 +3,7 @@
 channel=$1
 
 if [[ "${channel}" == "laster13" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/laster13/rdt-client/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/laster13/rdt-client/commits/master" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/rogerfar/rdt-client/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
