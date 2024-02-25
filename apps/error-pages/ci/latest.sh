3fde700b41ab6296e097d56e52e86e6fd251583d
David Young <davidy@funkypenguin.co.nz>
2024-02-26 12:39:53 +1300

    bump
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/error-pages/ci/latest.sh
+++ apps/error-pages/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET https://api.github.com/repos/tarampampam/error-pages/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*v}"
+# version="${version#*v}"
 printf "%s" "${version}"
