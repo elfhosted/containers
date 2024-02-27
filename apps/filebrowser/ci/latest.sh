bd7524520a47c1f9a8b301fb40f2a7b5bcadccad
David Young <davidy@funkypenguin.co.nz>
2024-02-27 18:42:56 +1300

    Stick with 2.23.0
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/filebrowser/ci/latest.sh
+++ apps/filebrowser/ci/latest.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/filebrowser/filebrowser/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+# version=$(curl -sX GET https://api.github.com/repos/filebrowser/filebrowser/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+# version="${version#*v}"
+# version="${version#*release-}"
+# printf "%s" "${version}"
+printf "2.23.0"
