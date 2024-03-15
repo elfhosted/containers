5ea32c18736a0c598fcc2c7c18b1cfb43fe81db2
David Young <davidy@funkypenguin.co.nz>
2024-03-15 13:16:59 +1300

    Fix rclone to 1.65.1 for now
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rclone/ci/latest.sh
+++ apps/rclone/ci/latest.sh
@@ -2,4 +2,5 @@
 version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=rclone&inrepo=alpine_edge" | jq -r '.rclone | .[] | select((.repo == "alpine_edge" and .binname == "rclone")) | .version')
 version="${version%%_*}"
 version="${version%%-*}"
-printf "%s" "${version}"
+# printf "%s" "${version}"
+print "1.65.1
