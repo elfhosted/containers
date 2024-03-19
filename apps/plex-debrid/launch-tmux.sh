6fe7c24e0930cf93bd648a336dbf0ad9b6f82c99
David Young <davidy@funkypenguin.co.nz>
2024-03-19 18:08:00 +1300

    try new plex-debrid design
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/plex-debrid/launch-tmux.sh
@@ -0,0 +1,3 @@
+#!/bin/ash
+
+tmux -f /usr/src/app/restricted.tmux.conf new-session -A -s plex_debrid /plex-debrid.sh
---
+++ apps/plex-debrid/plex-debrid.sh
@@ -0,0 +1,6 @@
+#!/bin/ash
+
+. /usr/src/app/plex_debrid/.venv/bin/activate
+cd /config/
+python /usr/src/app/plex_debrid/main.py --c
+
