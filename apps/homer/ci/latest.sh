8e17d288313533f69255f1a08b4ad2ccf081d891
David Young <davidy@funkypenguin.co.nz>
2024-03-23 14:13:37 +1300

    Fix homer image
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/homer/ci/latest.sh
+++ apps/homer/ci/latest.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET https://api.github.com/repos/bastienwirtz/homer/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*v}"
-version="${version#*release-}"
 printf "%s" "${version}"
