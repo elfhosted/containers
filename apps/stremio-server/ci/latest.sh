4a409ca198bf41ba4636bc69496df1225f4e6b11
David Young <davidy@funkypenguin.co.nz>
2024-03-28 11:54:18 +1300

    Try adding stremio-server
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/stremio-server/ci/latest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# version=$(curl -sX GET https://api.github.com/repos/mhdzumair/MediaFusion/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+# version="${version#*v}"
+# printf "%s" "${version}"
+print "v4.20.8" # hard-coded for now
