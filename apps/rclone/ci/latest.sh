03152d7c6958acb4f970cd087533812b525b5e60
David Young <davidy@funkypenguin.co.nz>
2024-04-06 08:15:57 +1300

    Hard-code latest rclone version for now
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rclone/ci/latest.sh
+++ apps/rclone/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=rclone&inrepo=alpine_edge" | jq -r '.rclone | .[] | select((.repo == "alpine_edge" and .binname == "rclone")) | .version')
-version="${version%%_*}"
-version="${version%%-*}"
-printf "%s" "${version}"
-
+# version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=rclone&inrepo=alpine_edge" | jq -r '.rclone | .[] | select((.repo == "alpine_edge" and .binname == "rclone")) | .version')
+# version="${version%%_*}"
+# version="${version%%-*}"
+# printf "%s" "${version}"
+printf "%s" "1.66.0" # the above link 403s currently
