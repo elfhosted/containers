fef208d4a2228903aabfe0a3ab585cad6aebdab9
David Young <davidy@funkypenguin.co.nz>
2024-04-06 19:55:03 +1300

    Build QSV
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/stremio-server/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# version=$(curl -sX GET https://api.github.com/repos/mhdzumair/MediaFusion/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-# version="${version#*v}"
-# printf "%s" "${version}"
-printf "%s" "v4.20.8" # hard-coded for now
