52ef8dda82d19b4c8c970f6bf154569b4c6ec88d
David Young <davidy@funkypenguin.co.nz>
2024-04-12 11:24:57 +1200

    refactor
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rdtclient/entrypoint.sh
+++ apps/rdtclient/entrypoint.sh
@@ -4,5 +4,10 @@
 source "/scripts/vpn.sh"
 
 #shellcheck disable=SC2086
+
+# We run from /tmp so that we can create a temporary folder for this, and leave /app as read-only
+cd /tmp
+cp /app/appsettings.json ./
+
 exec \
-    dotnet RdtClient.Web.dll
+    dotnet /app/RdtClient.Web.dll
