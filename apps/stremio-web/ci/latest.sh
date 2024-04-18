a64959a74735e003e31999e3de1ae8cf6db67793
David Young <davidy@funkypenguin.co.nz>
2024-04-18 22:17:08 +1200

    Bump stremio-web
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/stremio-web/ci/latest.sh
+++ apps/stremio-web/ci/latest.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-# version=$(curl -sX GET https://api.github.com/repos/mhdzumair/MediaFusion/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-# version="${version#*v}"
-# printf "%s" "${version}"
-printf "%s" "v5.0.0-beta.0" # hard-coded for now
+version=$(curl -sX GET https://api.github.com/repos/Stremio/stremio-web/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+printf "%s" "${version}"
