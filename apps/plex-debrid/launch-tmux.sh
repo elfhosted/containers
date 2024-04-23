13f93d590524b14bb24ba7544fa375863a2f5d20
David Young <davidy@funkypenguin.co.nz>
2024-04-23 22:05:11 +1200

    try to make PD run in the background
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plex-debrid/launch-tmux.sh
+++ apps/plex-debrid/launch-tmux.sh
@@ -1,4 +1,7 @@
 #!/bin/ash
+
+tmux -f /usr/src/app/restricted.tmux.conf new-session -A -d -s plex_debrid /plex-debrid.sh
+
 echo "Hit enter to start (or wait 5 min for auto-start, which will result in garbage printed to the screen until you hit enter)..."
 read -t 300
 tmux -f /usr/src/app/restricted.tmux.conf new-session -A -s plex_debrid /plex-debrid.sh
