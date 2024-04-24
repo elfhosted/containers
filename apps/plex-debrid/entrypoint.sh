a229716d98a15f62258973808fac40395fb76812
David Young <davidy@funkypenguin.co.nz>
2024-04-24 17:27:43 +1200

    Try to improve plex_debrid
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plex-debrid/entrypoint.sh
+++ apps/plex-debrid/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/ash
 
+tmux -f /usr/src/app/restricted.tmux.conf new-session -A -d -s plex_debrid /plex-debrid.sh
+
 pnpm start --command launch-tmux.sh --base / --title "plex_debrid | ElfHosted"
