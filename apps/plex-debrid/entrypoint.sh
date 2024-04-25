e1f86b6b15e2c81b02663eb3e147f15d3c02ba93
David Young <davidy@funkypenguin.co.nz>
2024-04-25 17:19:28 +1200

    fix bad plex_debrid update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plex-debrid/entrypoint.sh
+++ apps/plex-debrid/entrypoint.sh
@@ -2,4 +2,4 @@
 
 tmux -f /usr/src/app/restricted.tmux.conf new-session -A -d -s plex_debrid /plex-debrid.sh
 
-pnpm start --command launch-tmux.sh --base / --title "plex_debrid | ElfHosted"
+pnpm start --command /launch-tmux.sh --base / --title "plex_debrid | ElfHosted"
