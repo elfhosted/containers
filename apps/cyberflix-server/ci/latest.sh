71523e7ebc7f834cb6a0fdb81c948f1e6968d697
David Young <davidy@funkypenguin.co.nz>
2024-04-27 22:40:56 +1200

    bump latest cyberflix
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/cyberflix-server/ci/latest.sh
+++ apps/cyberflix-server/ci/latest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ "${channel}" == "develop" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/marcojoao/cyberflix-server/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-else
     version=$(curl -sX GET "https://api.github.com/repos/marcojoao/cyberflix-server/commits/develop" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET "https://api.github.com/repos/marcojoao/cyberflix-server/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 fi
 printf "%s" "${version}"
