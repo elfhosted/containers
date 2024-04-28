c27329566cc384463f51e207e0b2e395792d2258
David Young <davidy@funkypenguin.co.nz>
2024-04-28 22:50:09 +1200

    improve calibre-web
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/calibre-web/entrypoint.sh
+++ apps/calibre-web/entrypoint.sh
@@ -3,9 +3,12 @@
 #shellcheck disable=SC1091
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 
+# copy app to tmp location to avoid readonlyrootfs
+cp /app/calibre-web /tmp -rfp
+
 #shellcheck disable=SC2086
 exec \
     /usr/bin/python3 \
-        /app/calibre-web/cps.py \
+        /tmp/calibre-web/cps.py \
             -p /config/app.db \
             -g /config/gd.db
