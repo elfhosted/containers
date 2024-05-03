52a579824552dd442f4dc3ec53e70f56345fd956
David Young <davidy@funkypenguin.co.nz>
2024-05-03 23:45:23 +1200

    try improved version of wetty+plex_debrid
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plex-debrid/launch-tmux.sh
+++ apps/plex-debrid/launch-tmux.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 
 
-echo "Hit enter to start (or wait 5 min for auto-start, which will result in garbage printed to the screen until you hit enter)..."
-read -t 300
+# echo "Hit enter to start (or wait 5 min for auto-start, which will result in garbage printed to the screen until you hit enter)..."
+# read -t 300
 tmux -f /usr/src/app/restricted.tmux.conf new-session -A -s plex_debrid /plex-debrid.sh
 
