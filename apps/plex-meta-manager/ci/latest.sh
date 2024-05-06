dddb9471126971f79c4858079607f3811588ea10
David Young <davidy@funkypenguin.co.nz>
2024-05-07 09:56:59 +1200

    Update PMM
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plex-meta-manager/ci/latest.sh
+++ apps/plex-meta-manager/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/meisnate12/Plex-Meta-Manager/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version=$(curl -sX GET https://api.github.com/repos/Kometa-Team/Kometa/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 version="${version#*release-}"
 printf "%s" "${version}"
