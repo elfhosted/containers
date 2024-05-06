b63da70ec512fd284e9c2b81dc99d1b3ebcfcad8
David Young <davidy@funkypenguin.co.nz>
2024-05-07 10:12:19 +1200

    Spoof user-agent to repology api
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/qbittorrent/ci/latest.sh
+++ apps/qbittorrent/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # version=$(curl -sX GET "https://pkgs.alpinelinux.org/packages?name=qbittorrent-nox&branch=v3.16&arch" | grep -oP '(?<=<td class="version">)[^<]*')
-version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=qbittorrent&inrepo=alpine_edge" | jq -r '.qbittorrent | .[] | select((.repo == "alpine_edge" and .binname == "qbittorrent-nox")) | .version')
+version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=qbittorrent&inrepo=alpine_edge" -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" | jq -r '.qbittorrent | .[] | select((.repo == "alpine_edge" and .binname == "qbittorrent-nox")) | .version')
 version="${version%%_*}"
 version="${version%%-*}"
 printf "%s" "${version}"
