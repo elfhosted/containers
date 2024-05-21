43b02eb37331f348128e55f6be4ff8836a1bfa66
David Young <davidy@funkypenguin.co.nz>
2024-05-21 19:08:26 +1200

    don't want dev versions!
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/peertube/ci/latest.sh
+++ apps/peertube/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -s -S "https://registry.hub.docker.com/v2/repositories/chocobozzz/peertube/tags/" | jq -r '."results"[]["name"]' | grep -v latest | head -1)
+version=$(curl -s -S "https://registry.hub.docker.com/v2/repositories/chocobozzz/peertube/tags/" | jq -r '."results"[]["name"]' | grep -vE "develop|latest" | head -1)
 printf "%s" "${version}"
