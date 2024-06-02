618cb5087d4bc3dda76a1c0ca97b3afd0676b16f
David Young <davidy@funkypenguin.co.nz>
2024-06-02 20:53:50 +1200

    Update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/zurg/ci/latest.sh
+++ apps/zurg/ci/latest.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-# version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg-testing/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-# version="${version#*release-}"
-# printf "%s" "${version}"
-printf "%s" "v0.9.3-hotfix.11"
+if [[ "${channel}" == "rc" ]]; then
+    version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+else
+    version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg-testing/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
+version="${version#*release-}"
+printf "%s" "${version}"
