32faba5292174650c007b79d6d22b6f39181d94f
David Young <davidy@funkypenguin.co.nz>
2024-06-03 16:07:02 +1200

    bump zurg versions
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/zurg/ci/latest.sh
+++ apps/zurg/ci/latest.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 if [[ "${channel}" == "rc" ]]; then
     version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+    print 'v0.10.0-rc.1'
 else
     version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg-testing/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+    version="${version#*release-}"
+    printf "%s" "${version}"
 fi
-version="${version#*release-}"
-printf "%s" "${version}"
