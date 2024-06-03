c03b9e96614560eb7dd7d85b3a4602efb1cf4799
David Young <davidy@funkypenguin.co.nz>
2024-06-03 16:14:22 +1200

    rename zurg-rc to zurg-dev
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/zurg/ci/latest.sh
+++ apps/zurg/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [[ "${channel}" == "rc" ]]; then
+if [[ "${channel}" == "dev" ]]; then
     version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
     print 'v0.10.0-rc.1'
 else
