b9170f1498437f8f0e6360eb5b1612d93ab9ada4
David Young <davidy@funkypenguin.co.nz>
2024-06-06 09:11:04 +1200

    remove broked packages
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/torrentio-consumer/ci/latest.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-version=$(curl -sX GET "https://api.github.com/repos/geek-cookbook/torrentio.elfhosted.com/commits/${channel}" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-printf "%s" "${version}"
