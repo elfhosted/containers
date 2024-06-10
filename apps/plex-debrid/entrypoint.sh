3d76698447a0a6064a2da07cde0da550c78996b8
David Young <davidy@funkypenguin.co.nz>
2024-06-10 23:11:43 +1200

    fix regression
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plex-debrid/entrypoint.sh
+++ apps/plex-debrid/entrypoint.sh
@@ -2,5 +2,4 @@
 
 tmux -f /usr/src/app/restricted.tmux.conf new-session -A -d -s plex_debrid /plex-debrid.sh
 
-cd /usr/src/app/
 yarn start --command /launch-tmux.sh --base / --title "plex_debrid | ElfHosted"
--- apps/riven/entrypoint.sh
+++ apps/riven/entrypoint.sh
@@ -2,4 +2,5 @@
 
 # tmux -f /usr/src/app/restricted.tmux.conf new-session -A -d -s riven /riven.sh
 
+cd /usr/src/app/
 yarn start --command /iceberg/launch-tmux.sh --base / --title "Riven | ElfHosted"
