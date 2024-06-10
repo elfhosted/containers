a3d6e25df00fe5c0fd7170e2118fc13ea4eaaaef
David Young <davidy@funkypenguin.co.nz>
2024-06-10 23:30:58 +1200

    Update riven image
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/riven/entrypoint.sh
+++ apps/riven/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-# tmux -f /usr/src/app/restricted.tmux.conf new-session -A -d -s riven /riven.sh
+tmux -f /usr/src/app/restricted.tmux.conf new-session -A -d -s riven /iceberg/riven.sh
 
 cd /usr/src/app/
 yarn start --command /iceberg/launch-tmux.sh --base / --title "Riven | ElfHosted"
