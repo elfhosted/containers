c7120602b14bf290a8308bad164daf22edc381e5
David Young <davidy@funkypenguin.co.nz>
2024-06-10 14:34:12 +1200

    build latest nextpvr
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/nextpvr/ci/latest.sh
+++ apps/nextpvr/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/jellyfin/jellyfin/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*v}"
-version="${version#*release-}"
+# version=$(curl -sX GET "https://api.github.com/repos/jellyfin/jellyfin/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+# version="${version#*v}"
+# version="${version#*release-}"
 # printf "%s" "${version}"
-print "v6.1.5.231022"
+printf "%s" v6.1.5.231022"
