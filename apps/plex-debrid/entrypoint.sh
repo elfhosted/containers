2f6c8e37e82558310e2ac0eb3c695b48eb59ee5f
David Young <davidy@funkypenguin.co.nz>
2024-06-10 22:52:35 +1200

    Update entrypoint for riven
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plex-debrid/entrypoint.sh
+++ apps/plex-debrid/entrypoint.sh
@@ -2,4 +2,5 @@
 
 tmux -f /usr/src/app/restricted.tmux.conf new-session -A -d -s plex_debrid /plex-debrid.sh
 
+cd /usr/src/app/
 yarn start --command /launch-tmux.sh --base / --title "plex_debrid | ElfHosted"
