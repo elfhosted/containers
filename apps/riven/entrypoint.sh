0c152c719993f973f38238df52bcf8c77f28ea14
David Young <davidy@funkypenguin.co.nz>
2024-06-11 13:55:46 +1200

    tidy up riven background
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/riven/entrypoint.sh
+++ apps/riven/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/ash
 
-cd /usr/src/app
-tmux -f /iceberg/restricted.tmux.conf new-session -A -d -s riven /iceberg/riven.sh
+# Start riven in the background
+tmux -f /iceberg/restricted.tmux.conf new-session -x 80 -y 24 -A -d -s riven /iceberg/riven.sh
 
-yarn start --command /iceberg/launch-tmux.sh --base / --title "Riven | ElfHosted"
-
-# wetty --command top --base / --title "Riven | ElfHosted"
+# Prepare a tmux entry to the already-running process
+ttyd -p 3001 -W -t titleFixed='Riven | ElfHosted' -t 'theme={"background": "#0B181C"}' -t drawBoldTextInBrightColors=false /iceberg/launch-tmux.sh
