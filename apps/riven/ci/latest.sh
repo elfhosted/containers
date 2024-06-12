db1d39db15121cd952d2f568ec3c0360b3dd7a39
David Young <davidy@funkypenguin.co.nz>
2024-06-12 17:22:52 +1200

    Update latest pull for riven
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/riven/ci/latest.sh
+++ apps/riven/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET "https://api.github.com/repos/dreulavelle/iceberg/commits/dev" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET "https://api.github.com/repos/rivenmedia/riven/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 printf "%s" "${version}"
