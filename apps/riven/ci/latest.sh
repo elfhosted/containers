57b20b168b8a006302f8192fea0b694b25bece7f
David Young <davidy@funkypenguin.co.nz>
2024-06-13 16:05:40 +1200

    bump test image
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/riven/ci/latest.sh
+++ apps/riven/ci/latest.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET "https://api.github.com/repos/rivenmedia/riven/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+channel=$1
+
+if [[ "${channel}" == "test" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/rivenmedia/riven/commits/fix_id" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET "https://api.github.com/repos/rivenmedia/riven/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+fi
+
 printf "%s" "${version}"
