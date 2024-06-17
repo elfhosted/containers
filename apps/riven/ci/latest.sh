a6e76d9cb1e178eaeed4f37883af6444ff710cce
David Young <davidy@funkypenguin.co.nz>
2024-06-17 16:23:59 +1200

    build standalone backend image
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/riven/ci/latest.sh
+++ apps/riven/ci/latest.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
 channel=$1
-
-if [[ "${channel}" == "test" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/rivenmedia/riven/commits/fix_id" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-else
-    version=$(curl -sX GET "https://api.github.com/repos/rivenmedia/riven/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-fi
+version=$(curl -sX GET "https://api.github.com/repos/rivenmedia/riven/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 
 printf "%s" "${version}"
