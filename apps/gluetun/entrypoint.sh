d994e3187d92766be71f7d7744fda719cd5aa906
David Young <davidy@funkypenguin.co.nz>
2024-06-19 11:31:00 +1200

    add sleep mode to gluetun
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/gluetun/entrypoint.sh
+++ apps/gluetun/entrypoint.sh
@@ -5,5 +5,12 @@ if [[ -f /shared/VPN_ENDPOINT_IP ]]; then
     export VPN_ENDPOINT_IP=$(cat /shared/VPN_ENDPOINT_IP)
 fi
 
-exec \
-    /gluetun-entrypoint
+# If we're in "sleep mode", then don't actually, just do nothing (useful when we control how a pod will run based on an env var)
+if [[ ! -z "$SLEEP_MODE" ]]; 
+then
+    echo "SLEEP_MODE env var set, doing nothing (you are probably using WARP).."
+    sleep infinity
+else
+    exec \
+        /gluetun-entrypoint
+fi
