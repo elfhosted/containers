e485efa5aaaaa0d323294777a549faf35aca5f70
David Young <davidy@funkypenguin.co.nz>
2024-06-19 12:21:21 +1200

    try building warp
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/warp/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/ash
+
+# If gluetun vars exist, then don't actually, just do nothing (useful when we control how a pod will run based on an env var)
+if [[ ! -z "$VPN_SERVICE_PROVIDER" ]]; 
+then
+    echo "VPN_SERVICE_PROVIDER env var set, doing nothing (you are probably using gluetun).."
+    sleep infinity
+else
+    exec \
+        /orig-entrypoint.sh
+fi
