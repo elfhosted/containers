39785324aa5b3e0a437799dfd1f535a4b289b352
David Young <davidy@funkypenguin.co.nz>
2024-06-21 18:50:59 +1200

    Update zurg image
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/zurg/ci/latest.sh
+++ apps/zurg/ci/latest.sh
@@ -4,6 +4,7 @@ channel=$1
 
 if [[ "${channel}" == "rc" ]]; then
     version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg/releases --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | first | .tag_name')
+    version="2024.06.20-nightly" # until current bug is fixed
 else
     version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg-testing/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
     version="${version#*release-}"
--- apps/zurg/entrypoint.sh
+++ apps/zurg/entrypoint.sh
@@ -3,6 +3,17 @@
 #shellcheck disable=SC1091
 source "/scripts/vpn.sh"
 
+if [[ "${WAIT_FOR_WARP:-"false"}" == "true" ]]; then
+    echo "Waiting for WARP to be connected..."
+    # Also account for gluetun-style http controller
+    if (timeout 2s curl --silent --socks5 127.0.0.1:1080 https://www.cloudflare.com | grep -q cloudflare ); then
+        break
+    fi    
+    echo "WARP not connected"
+    sleep 2
+    echo "WARP Connected, starting application..."
+fi
+
 cd /config
 #shellcheck disable=SC2086
 exec \
