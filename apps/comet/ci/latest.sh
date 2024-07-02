ce5d09f64976a53962b649acf2e8c8c019928119
David Young <davidy@funkypenguin.co.nz>
2024-07-02 21:55:45 +1200

    update comet
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/comet/ci/latest.sh
+++ apps/comet/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/g0ldyy/annatar/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
+version="$(curl -sX GET "https://api.github.com/repos/g0ldyy/comet/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
 printf "%s" "${version}"
