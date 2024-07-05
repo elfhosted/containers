b2179905c31e0d8eba8285904f4ac8207beacda1
David Young <davidy@funkypenguin.co.nz>
2024-07-05 23:52:59 +1200

    bump comet
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/formulio/ci/latest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET https://api.github.com/repos/aymene69/stremio-jackett/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+# version=$(curl -sX GET "https://api.github.com/repos/aymene69/stremio-jackett/commits/dev" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+
+printf "%s" "${version}"
