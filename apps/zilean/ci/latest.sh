80ffd6662a96c1ec499a2edee7919a22e7dbc953
David Young <davidy@funkypenguin.co.nz>
2024-07-08 02:17:52 +1200

    update zilean to build off releases
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/zilean/ci/latest.sh
+++ apps/zilean/ci/latest.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/iPromKnight/zilean/commits/master" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET https://api.github.com/repos/iPromKnight/zilean/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+
 printf "%s" "${version}"
