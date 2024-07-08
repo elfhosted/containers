db1b5af835edbd752c9bc0be54eb18158cc1a14b
David Young <davidy@funkypenguin.co.nz>
2024-07-08 21:49:45 +1200

    improve kometa menu
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/kometa/kometa.sh
+++ apps/kometa/kometa.sh
@@ -9,9 +9,13 @@ if [ $? -eq 0 ]; then
     echo "You pressed a key! Dropping to shell.."
 
     items=(1 "Run everything now!"
-        2 "Run overlays now"
-        3 "Run metadata now"
+        2 "Resume latest run"
+        3 "Run overlays now"
         4 "Run collections now"
+        5 "Run operations now"
+        6 "Run playlists now"
+        7 "Run metadata now"
+        8 "Run on schedule (12PM UTC)"
         )
 
     while choice=$(dialog --title "$TITLE" \
@@ -20,18 +24,21 @@ if [ $? -eq 0 ]; then
         do
         case $choice in
             1) KOMETA_RUN=true python3 kometa.py ;;
-            2) KOMETA_RUN=true KOMETA_OVERLAYS_ONLY=true python3 kometa.py ;;
-            3) KOMETA_RUN=true KOMETA_METADATA_ONLY=true python3 kometa.py ;;
+            2) KOMETA_RESUME=true python3 kometa.py ;; 
+            3) KOMETA_RUN=true KOMETA_OVERLAYS_ONLY=true python3 kometa.py ;;            
             4) KOMETA_RUN=true KOMETA_COLLECTIONS_ONLY=true python3 kometa.py ;;
-            5) ;; # some action on 2                
+            5) KOMETA_RUN=true KOMETA_OPERATIONS_ONLY=true python3 kometa.py ;; 
+            6) KOMETA_RUN=true KOMETA_PLAYLISTS_ONLY=true python3 kometa.py ;; 
+            7) KOMETA_RUN=true KOMETA_METADATA_ONLY=true python3 kometa.py ;;           
+            8) KOMETA_TIMES=12:00 python3 kometa.py ;;              
             *) ;; # some action on other
         esac
     done
     clear # clear after user pressed Cancel
 
 else
-    echo "Timeout reached, running KOMETA as usual..."
-    KOMETA_RUN=true python3 kometa.py
+    echo "Timeout reached, running KOMETA as on schedule (12PM UTC)..."
+    KOMETA_TIMES=12:00 python3 kometa.py
 fi
 
 
