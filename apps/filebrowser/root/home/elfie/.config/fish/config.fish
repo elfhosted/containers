30fa5d6f618ad12f078362ae853f4a894f584eb9
David Young <davidy@funkypenguin.co.nz>
2024-07-11 17:12:06 +1200

    tweak login
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/filebrowser/root/home/elfie/.config/fish/config.fish
+++ apps/filebrowser/root/home/elfie/.config/fish/config.fish
@@ -1,1 +1,1 @@
-set -U fish_greeting "Welcome to the ElfTerm! For help, see https://elfhosted.com/how-to-use/elfterm/"
+set -U fish_greeting "Welcome to the ElfTerm! For help, see https://elfhosted.com/how-to/use-elfterm/"
