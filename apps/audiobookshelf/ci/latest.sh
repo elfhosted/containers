7dad53ef3af847463e9ccda2ab0e0bc235e4f0e4
David Young <davidy@funkypenguin.co.nz>
2024-07-18 20:24:16 +1200

    bump
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/audiobookshelf/ci/latest.sh
+++ apps/audiobookshelf/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET https://api.github.com/repos/advplyr/audiobookshelf/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*v}"
+# version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
