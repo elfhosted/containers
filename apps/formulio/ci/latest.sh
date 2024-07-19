7d1663acc9e1affc2a9040c1ea8e36539f369bcf
David Young <davidy@funkypenguin.co.nz>
2024-07-19 23:43:49 +1200

    add formulio
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/formulio/ci/latest.sh
+++ apps/formulio/ci/latest.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/aymene69/stremio-jackett/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-# version=$(curl -sX GET "https://api.github.com/repos/aymene69/stremio-jackett/commits/dev" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-
+version=$(curl -sX GET "https://api.github.com/repos/TheRaceDirector/formuliodev/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 printf "%s" "${version}"
