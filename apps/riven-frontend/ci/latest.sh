ca8fd7d93b7bf0a65e9f18673864ef07f9567a18
David Young <davidy@funkypenguin.co.nz>
2024-07-24 08:54:22 +1200

    switch riven-frontend to releases
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/riven-frontend/ci/latest.sh
+++ apps/riven-frontend/ci/latest.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 channel=$1
-version=$(curl -sX GET "https://api.github.com/repos/rivenmedia/riven-frontend/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-
+version="$(curl -sX GET "https://api.github.com/repos/rivenmedia/riven-frontend/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
 printf "%s" "${version}"
