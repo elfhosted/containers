bb051483419e7e0d45fa27a7a900d23e4eb257db
David Young <davidy@funkypenguin.co.nz>
2024-07-25 16:46:47 +1200

    remove --no-modtime from webdav
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rclone/rootfs/webdav.sh
+++ apps/rclone/rootfs/webdav.sh
@@ -6,7 +6,6 @@ cd /storage
     --user ${WEBDAV_USERNAME} \
     --pass "${WEBDAV_PASSWORD}" \
     --max-header-bytes=8192 \
-    --no-modtime \
     --vfs-read-chunk-size=10M \
     --vfs-read-chunk-size-limit=10M \
     --stats 60s \
