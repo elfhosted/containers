6698d08361c526547cf68d4c5c7a3d922646dc11
David Young <davidy@funkypenguin.co.nz>
2024-08-03 13:25:56 +1200

    bump to releases from ENP
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/stremio-easynews-addon/ci/latest.sh
+++ apps/stremio-easynews-addon/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET "https://api.github.com/repos/sleeyax/stremio-easynews-addon/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET https://api.github.com/repos/sleeyax/stremio-easynews-addon/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"
