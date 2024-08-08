389ac6e1e595c70f92715f2bc3485ce1333eab33
David Young <davidy@funkypenguin.co.nz>
2024-08-09 01:04:18 +1200

    don't expose webdav without password
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rclone/rootfs/webdav.sh
+++ apps/rclone/rootfs/webdav.sh
@@ -1,6 +1,8 @@
 #!/bin/ash
 
 cd /storage
+
+if [[ ! -z "${WEBDAV_PASSWORD}" ]]; then
 /usr/bin/rclone serve webdav \
     --addr :5574 \
     --user ${WEBDAV_USERNAME} \
@@ -12,3 +14,18 @@ cd /storage
     --copy-links \
     -v \
     /storage
+else
+    mkdir /tmp/no-password
+    echo "WebDAV password not set. See https://elfhosted.com/app/webdav/ for instructions" > /tmp/no-password/README.txt
+    /usr/bin/rclone serve webdav \
+    --addr :5574 \
+    --user ${WEBDAV_USERNAME} \
+    --pass "${WEBDAV_PASSWORD}" \
+    --max-header-bytes=8192 \
+    --vfs-read-chunk-size=10M \
+    --vfs-read-chunk-size-limit=10M \
+    --stats 60s \
+    --copy-links \
+    -v \
+    /tmp/no-password    
+fi
