6e0c5ea3d9922e50411f43b03bac014c022a624e
David Young <davidy@funkypenguin.co.nz>
2024-08-10 14:17:40 +1200

    update for gluetun control server issue
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/alpine/scripts/vpn.sh
+++ apps/alpine/scripts/vpn.sh
@@ -3,8 +3,8 @@
 if [[ "${WAIT_FOR_VPN:-"false"}" == "true" ]]; then
     echo "Waiting for VPN to be connected..."
     while ! grep -s -q "connected" /shared/vpnstatus; do
-        # Also account for gluetun-style http controller
-        if (timeout 2s curl -s http://localhost:8000/v1/openvpn/status | grep -q running); then
+        # Also account for gluetun-style http controller - confirm that our public IP exists and doesn't belong to hetzner
+        if (timeout 2s curl -s http://localhost:8000/v1/publicip/ip | grep public_ip | -vi hetzner); then
             break
         fi    
         echo "VPN not connected"
