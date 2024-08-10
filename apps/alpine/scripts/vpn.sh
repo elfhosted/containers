06d6669a7a5aaa9bbf5e5a5e3cd3b4ac1849f897
David Young <davidy@funkypenguin.co.nz>
2024-08-10 14:23:05 +1200

    fix
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/alpine/scripts/vpn.sh
+++ apps/alpine/scripts/vpn.sh
@@ -3,8 +3,8 @@
 if [[ "${WAIT_FOR_VPN:-"false"}" == "true" ]]; then
     echo "Waiting for VPN to be connected..."
     while ! grep -s -q "connected" /shared/vpnstatus; do
-        # Also account for gluetun-style http controller - confirm that our public IP exists and doesn't belong to hetzner
-        if (timeout 2s curl -s http://localhost:8000/v1/publicip/ip | grep public_ip | -vi hetzner); then
+        # Also account for gluetun-style http controller - undocumented feature from https://github.com/qdm12/gluetun/issues/2277#issuecomment-2115964521
+        if (timeout 2s curl -s http://localhost:8000/v1/vpn/status | grep -q running); then
             break
         fi    
         echo "VPN not connected"
