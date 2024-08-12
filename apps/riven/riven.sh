9aac99f4c9bb920b06a46ff9ed12587331f0febc
David Young <davidy@funkypenguin.co.nz>
2024-08-12 16:49:29 +1200

    stability fix
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/riven/riven.sh
+++ apps/riven/riven.sh
@@ -2,25 +2,10 @@
 
 cd /riven/src
 
-echo "💥 For the option to reset your database, press the 'x' key within 10 seconds.."
-
-# Read a single character with a timeout of 10 seconds
-read -n 1 -t 10 key
-clear # clear after user pressed Cancel
-
-# Check if the key pressed is 'x'
-if [[ $key == "x" ]]; then
-    read -p "Wipe Riven's database first (y/n)?" choice
-    case "$choice" in 
-    y|Y ) 
-        python main.py --hard_reset_db
-        # workaround
-        PGPASSWORD=postgres psql -U postgres -d riven -h localhost -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
-        ;;
-    * ) ;; # do nothing
-    esac
-    clear # clear after user pressed Cancel
-fi
+echo "📺 Resetting database (until alembic migrations are fixed)..."
+python main.py --hard_reset_db
+PGPASSWORD=postgres psql -U postgres -d riven -h localhost -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+rm -rf /riven/alembic
 
 echo "📺 Waiting for plex to be up..."
 /usr/local/bin/wait-for -t 3600 plex:32400 -- echo "✅"
