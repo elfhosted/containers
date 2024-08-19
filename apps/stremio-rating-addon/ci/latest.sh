15f507226b61a7bcde05fbc2dd653fc0e7e927c3
David Young <davidy@funkypenguin.co.nz>
2024-08-20 10:24:42 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/stremio-rating-addon/ci/latest.sh
+++ apps/stremio-rating-addon/ci/latest.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-
-if [[ "${channel}" == "develop" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/marcojoao/cyberflix-server/commits/develop" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-else
-    version=$(curl -sX GET "https://api.github.com/repos/marcojoao/cyberflix-server/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-fi
+version=$(curl -sX GET "https://api.github.com/repos/exdecimal16/stremio-rating-addon/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 printf "%s" "${version}"
