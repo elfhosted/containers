14a4790067f2ccafbdb9a2f9678c2e2a53002190
David Young <davidy@funkypenguin.co.nz>
2024-08-23 11:05:06 +1200

    tweak
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/librespeed/ci/latest.sh
+++ apps/librespeed/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos//librespeed/speedtest/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version=$(curl -sX GET "https://api.github.com/repos/librespeed/speedtest/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
