877bb69d03260596c84c914c86bc0a47a9c4d392
David Young <davidy@funkypenguin.co.nz>
2024-08-24 00:17:28 +1200

    Add west's blackhole script
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/wests-blackhole-script/ci/latest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET https://api.github.com/repos/westsurname/scripts/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version="${version#*release-}"
+printf "%s" "${version}"
