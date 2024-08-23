23a1e59d6fdb73e96ffcb263810324b7723556f7
David Young <davidy@funkypenguin.co.nz>
2024-08-24 10:10:18 +1200

    make blackhole fail gracefully
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/wests-blackhole-script/blackhole.sh
+++ apps/wests-blackhole-script/blackhole.sh
@@ -6,3 +6,5 @@ export $(cat /config/blackhole.env | grep -vE "^#" | xargs)
 # run the script
 cd /app
 python blackhole_watcher.py
+echo "Blackhole has unexpectedly exited :( Press any key to restart, or wait 5 min... (incase you need to capture debug output)"
+read -s -n 1 -t 300
