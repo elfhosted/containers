a2be16720035c486efd950abfcecdd6d8bca5564
David Young <davidy@funkypenguin.co.nz>
2024-09-04 22:06:37 +1200

    add sanity to blackhole launcher
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/wests-blackhole-script/blackhole.sh
+++ apps/wests-blackhole-script/blackhole.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# get our env vars from persistent storage
-export $(cat /config/blackhole.env | grep -vE "^#" | xargs)
+if [[ -z "${REALDEBRID_API_KEY}" && ${REALDEBRID_ENABLED+x} ]]; then
+  echo "RealDebrid enabled (default) but REALDEBRID_API_KEY env var is not set"
+  echo "Use ElfTerm to run 'elfbot env blackhole REALDEBRID_API_KEY=<your key>'"
+  echo -e "\n\n NOTE: Blackhole is not REQUIRED. It's an optional alternative to RDTClient. You could just ignore this"
+  sleep infinity
+elif [[ -z "${TORBOX_API_KEY}" && ${TORBOX_ENABLED+x} ]]; then
+  echo "Torbox enabled (manually) but TORBOX_API_KEY env var is not set"
+  echo "Use ElfTerm to run 'elfbot env blackhole TORBOX_API_KEY=<your key>'"
+  echo -e "\n\n NOTE: Blackhole is not REQUIRED. It's an optional alternative to RDTClient. You could just ignore this"
+  sleep infinity
+fi
 
 # run the script
 cd /app
