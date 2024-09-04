38200aefab4732c58091be9024292c5ebb00c927
David Young <davidy@funkypenguin.co.nz>
2024-09-04 22:07:07 +1200

    tidy up
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/wests-blackhole-script/blackhole.sh
+++ apps/wests-blackhole-script/blackhole.sh
@@ -3,12 +3,12 @@
 if [[ -z "${REALDEBRID_API_KEY}" && ${REALDEBRID_ENABLED+x} ]]; then
   echo "RealDebrid enabled (default) but REALDEBRID_API_KEY env var is not set"
   echo "Use ElfTerm to run 'elfbot env blackhole REALDEBRID_API_KEY=<your key>'"
-  echo -e "\n\n NOTE: Blackhole is not REQUIRED. It's an optional alternative to RDTClient. You could just ignore this"
+  echo -e "\n\nNOTE: Blackhole is not REQUIRED. It's an optional alternative to RDTClient. You could just ignore this"
   sleep infinity
 elif [[ -z "${TORBOX_API_KEY}" && ${TORBOX_ENABLED+x} ]]; then
   echo "Torbox enabled (manually) but TORBOX_API_KEY env var is not set"
   echo "Use ElfTerm to run 'elfbot env blackhole TORBOX_API_KEY=<your key>'"
-  echo -e "\n\n NOTE: Blackhole is not REQUIRED. It's an optional alternative to RDTClient. You could just ignore this"
+  echo -e "\n\nNOTE: Blackhole is not REQUIRED. It's an optional alternative to RDTClient. You could just ignore this"
   sleep infinity
 fi
 
