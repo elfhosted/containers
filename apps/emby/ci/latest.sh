c3731939e0513f0eaf229e348373c08859a37670
David Young <davidy@funkypenguin.co.nz>
2024-09-14 22:21:13 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/emby/ci/latest.sh
+++ apps/emby/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/MediaBrowser/Emby.Releases/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq -r '.[] | select(.prerelease == true) | .tag_name' | head -n 1)
+version=$(curl -sX GET https://api.github.com/repos/MediaBrowser/Emby.Releases/releases --header "Authorization: Bearer ${TOKEN}" | jq -r '.[] | select(.prerelease == true) | .tag_name' | head -n 1)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
