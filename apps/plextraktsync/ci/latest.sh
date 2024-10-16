c9e698b93fee36c7a875e71fa81085c51951c270
David Young <davidy@funkypenguin.co.nz>
2024-10-16 16:33:09 +1300

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plextraktsync/ci/latest.sh
+++ apps/plextraktsync/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/Taxel/PlexTraktSync/tags --header "Authorization: Bearer ${TOKEN}"grep '"name"' | head -n 1 | sed 's/.*: "\(.*\)",/\1/')
+version=$(curl -sX GET https://api.github.com/repos/Taxel/PlexTraktSync/tags --header "Authorization: Bearer ${TOKEN}" | grep '"name"' | head -n 1 | sed 's/.*: "\(.*\)",/\1/')
 printf "%s" "${version}"
