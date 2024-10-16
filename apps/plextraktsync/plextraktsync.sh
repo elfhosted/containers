f84cb0bfedc643c4541e05604991a70bdc75dc85
David Young <davidy@funkypenguin.co.nz>
2024-10-17 00:05:23 +1300

    prompt for plextraktsync to be setup
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plextraktsync/plextraktsync.sh
+++ apps/plextraktsync/plextraktsync.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-/home/elfie/.local/bin/plextraktsync watch
+if grep -q plex:32400 /home/elfie/.config/PlexTraktSync/servers.yml; then
+    /home/elfie/.local/bin/plextraktsync watch
+else
+    echo "PlexTraktSync not setup for http://plex:32400, please use ElfTerm to configure it first"
+fi
+read -n 1 -s -r -p "Press any key to continue"
