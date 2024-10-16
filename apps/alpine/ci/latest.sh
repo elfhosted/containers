2b635be6f75f8ae27e15ee46459eb8416c991da4
David Young <davidy@funkypenguin.co.nz>
2024-10-17 09:47:08 +1300

    Fix constant alpine builds
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/alpine/ci/latest.sh
+++ apps/alpine/ci/latest.sh
@@ -3,9 +3,10 @@ channel=$1
 if [[ "${channel}" == "node" ]]; then
     version="20"
 else
-    version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/alpine/tags?ordering=name&name=$channel" | jq --raw-output --arg s "$channel" '.results[] | select(.name | contains($s)) | .name'  | head -n1)
-    version="${version#*v}"
-    version="${version#*release-}"
+    version=$channel # in every other case, the channel _is_ the version
+    # version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/alpine/tags?ordering=name&name=$channel" | jq --raw-output --arg s "$channel" '.results[] | select(.name | contains($s)) | .name'  | head -n1)
+    # version="${version#*v}"
+    # version="${version#*release-}"
 fi
 printf "%s" "${version}"
 
