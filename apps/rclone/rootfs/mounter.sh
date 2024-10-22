0d4ca513a4708fd01ffd57bbfabbc03341452d06
David Young <davidy@funkypenguin.co.nz>
2024-10-22 23:52:19 +1300

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rclone/rootfs/mounter.sh
+++ apps/rclone/rootfs/mounter.sh
@@ -10,13 +10,16 @@ done
 
 # run once the first time
 sleep 10s # wait for rclone to be running
-refresh_mounts
 
-# Loop indefinitely, monitoring for changes
-while true; do
-    # Wait for any changes to the file
-    inotifywait -e modify "/config/rclone.conf"
-    
-    # Call the function when the file is modified
+if [ -f /config/rclone.conf ]; then
     refresh_mounts
-done
+
+    # Loop indefinitely, monitoring for changes
+    while true; do
+        # Wait for any changes to the file
+        inotifywait -e modify "/config/rclone.conf"
+        
+        # Call the function when the file is modified
+        refresh_mounts
+    done
+fi
