eb2aa38d39814e9e6c7588d6928507ac6d6eb85a
David Young <davidy@funkypenguin.co.nz>
2024-10-26 08:05:08 +1300

    suggestarr uses releases now
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/suggestarr/ci/latest.sh
+++ apps/suggestarr/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/giuseppe99barchetta/SuggestArr/commits/master" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET https://api.github.com/repos/giuseppe99barchetta/SuggestArr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"
