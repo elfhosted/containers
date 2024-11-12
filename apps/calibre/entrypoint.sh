6dbf9f0edda123e4b059bc2c932241a75e48bb61
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2024-11-12 20:15:34 +0200

    calibre: make entrypoint more generic

--- apps/calibre/entrypoint.sh
+++ apps/calibre/entrypoint.sh
@@ -4,9 +4,18 @@
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 
 #shellcheck disable=SC2086
-XDG_RUNTIME_DIR=/tmp/runtime-root /usr/bin/calibre-server \
-    --disable-use-bonjour \
+export XDG_RUNTIME_DIR=/tmp/runtime-root 
+export CALIBRE_SERVER_BOOKS_PATH="${CALIBRE_SERVER_BOOKS_PATH:="/media/books"}"
+# Calibre will not start unless there is a database
+if [ ! -f "${CALIBRE_SERVER_BOOKS_PATH}/metadata.db" ]; then
+    echo "Creating an empty calibre metadata database at [${CALIBRE_SERVER_BOOKS_PATH}/metadata.db]"
+    touch "${CALIBRE_SERVER_BOOKS_PATH}/metadata.db"
+fi
+
+/usr/bin/calibre-server \
+    --port="${CALIBRE_SERVER_PORT:-8080}" \
     --enable-local-write \
-    --trusted-ips="10.0.0.0/8" \
+    --disable-use-bonjour \
+    --trusted-ips="${CALIBRE_SERVER_TRUSTED_IPS:-'192.168.0.0/16,172.16.0.0/12,10.0.0.0/8'}" \
     "$@" \
-    "/storage/elfstorage/books"
+    "${CALIBRE_SERVER_BOOKS_PATH}"
