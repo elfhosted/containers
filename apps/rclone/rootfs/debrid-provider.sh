de1f371e7d43d36a5087cc7a6e846be614f72cc3
David Young <davidy@funkypenguin.co.nz>
2024-11-22 17:08:52 +1300

    streamline debrid provider in rclone
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/rclone/rootfs/debrid-provider.sh
@@ -0,0 +1,18 @@
+#!/bin/ash
+
+/usr/bin/rclone serve webdav \
+    --config /rlcone-debrid-provider.conf \
+    --addr :9999 \
+    --max-header-bytes=8192 \
+    --no-modtime \
+    --vfs-read-chunk-size=10M \
+    --vfs-read-chunk-size-limit=10M \
+    --dir-cache-time 10s \
+    --multi-thread-streams=0 \
+    --cutoff-mode=cautious \
+    --vfs-cache-mode writes \
+    --network-mode \
+    --buffer-size=0
+    -v \
+    debrid-provider:
+
