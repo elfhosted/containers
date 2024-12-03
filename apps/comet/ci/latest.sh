2018e215d2ba41632b07852cd95f54ce5bba1639
David Young <davidy@funkypenguin.co.nz>
2024-12-04 09:27:14 +1300

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/comet/ci/latest.sh
+++ apps/comet/ci/latest.sh
@@ -2,8 +2,8 @@
 
 channel=$1
 
-if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/g0ldyy/comet/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+if [[ "${channel}" == "rewrite" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/g0ldyy/comet/commits/rewrite" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version="$(curl -sX GET "https://api.github.com/repos/g0ldyy/comet/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
 fi
