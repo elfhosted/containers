8a72bf1106cc57a66377df859d43e9981bbf1d76
David Young <davidy@funkypenguin.co.nz>
2024-12-07 10:15:40 +1300

    update stremthru

--- apps/stremthru/ci/latest.sh
+++ apps/stremthru/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/iPromKnight/zilean/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version=$(curl -sX GET https://api.github.com/repos/MunifTanjim/stremthru/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 
 printf "%s" "${version}"
