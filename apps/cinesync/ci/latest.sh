493689b15afddfbedbbb85eacb4a6fa365f29c9e
David Young <davidy@funkypenguin.co.nz>
2025-01-02 13:02:14 +1300

    bump

--- apps/cinesync/ci/latest.sh
+++ apps/cinesync/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET https://api.github.com/repos/sureshfizzy/CineSync/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*release-}"
+version="${version#*CineSync-}"
 printf "%s" "${version}"    
 
