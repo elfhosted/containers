e0b74f36e8b10311618a9202aa2cb641016f05a3
David Young <davidy@funkypenguin.co.nz>
2025-01-04 16:57:43 +1300

    bump seerrbridge
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/seerrbridge/seerrbridge.sh
+++ apps/seerrbridge/seerrbridge.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# if [[ -z "$PLEX_TOKEN" ]]; 
-# then
-#     echo "seerrbridge is an alternate way to consume your debrid media in Plex"
-#     echo "You don't HAVE to configure this, it's harmless to ignore it"
-#     echo "To activate seerrbridge, you'll need to point your Plex Libraries to:"
-#     echo "  /storage/symlinks/seerrbridge/Movies"
-#     echo "  /storage/symlinks/seerrbridge/Series"
-#     echo "And then use https://plex-token-generator.elfhosted.com to generate a token, and add it"
-#     echo "by running 'elfbot env seerrbridge PLEX_TOKEN=<token>' in ElfTerm, and waiting for seerrbridge to restart"
-#     sleep infinity 
-# fi
+if [[ -z "$OVERSEERR_API_KEY" || -z "$TRAKT_API_KEY" ]]; 
+then
+    echo "SeerrBridge uses DebridMediaManager to fulfill Overseerr requests"
+    echo "Before SeerBridge will run you, need to define your OVERSEERR_API_KEY, your TRAKT_API_KEY, and"
+    echo "your RealDebrid/DMM credentials"
+    echo "See https://docs.elfhosted.com/app/seerrbridge for further details"
+    sleep infinity 
+fi
 
 .local/bin/uvicorn seerrbridge:app --host 0.0.0.0 --port 8777
-
 
 echo "seerrbridge has unexpectedly exited :( Press any key to restart, or wait 5 min... (incase you need to capture debug output)"
 read -s -n 1 -t 300
