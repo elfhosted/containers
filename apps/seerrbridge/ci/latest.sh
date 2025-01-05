54a4293a535da5240ef8dde955eae0bbf6fcfcf0
David Young <davidy@funkypenguin.co.nz>
2025-01-06 11:00:04 +1300

    build latest beta
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/seerrbridge/ci/latest.sh
+++ apps/seerrbridge/ci/latest.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET https://api.github.com/repos/Woahai321/SeerrBridge/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-printf "%s" "${version}"    
+# printf "%s" "${version}"    
 
+print "v0.4.5-beta"
