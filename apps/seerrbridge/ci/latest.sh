3bee47f6e6d3f5cb273161db0807469ce49a384c
David Young <davidy@funkypenguin.co.nz>
2025-01-06 11:42:22 +1300

    add seerrbridge dev branch
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/seerrbridge/ci/latest.sh
+++ apps/seerrbridge/ci/latest.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET https://api.github.com/repos/Woahai321/SeerrBridge/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-# printf "%s" "${version}"    
+printf "%s" "${version}"    
 
-printf "v0.4.5-beta"
