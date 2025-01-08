cbf36bc2f388414cc8ef816302f42c6543ac8632
David Young <davidy@funkypenguin.co.nz>
2025-01-08 15:02:38 +1300

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/cli_debrid/ci/latest.sh
+++ apps/cli_debrid/ci/latest.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET https://api.github.com/repos/godver3/cli_debrid/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*v}"
-version="${version#*release-}"
 printf "%s" "${version}"
