ba163382e291ef4976b17cffc0e8912cfa088656
David Young <davidy@funkypenguin.co.nz>
2025-01-10 08:48:49 +1300

    add dev version of cli_debrid
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/cli_debrid/ci/latest.sh
+++ apps/cli_debrid/ci/latest.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/godver3/cli_debrid/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET https://api.github.com/repos/godver3/cli_debrid/releases --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.[0].tag_name')
+else
+    version=$(curl -sX GET https://api.github.com/repos/godver3/cli_debrid/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
 printf "%s" "${version}"
