171286e2d7c25b061e0a88e3a536ff0f44493f23
David Young <davidy@funkypenguin.co.nz>
2025-01-16 15:59:38 +1300

    update source repo

--- apps/youriptv/ci copy/latest.sh
+++ apps/youriptv/ci copy/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET "https://api.github.com/repos/mik25/epgtv/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET "https://api.github.com/repos/mikmc55/epgtv/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 printf "%s" "${version}"
