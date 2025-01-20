6b456c598c75733078b05b7485a4a15326747fa6
David Young <davidy@funkypenguin.co.nz>
2025-01-21 10:02:31 +1300

    update debridav to v0.2.0

--- apps/debridav/ci/latest.sh
+++ apps/debridav/ci/latest.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/skjaere/debridav/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET https://api.github.com/repos/skjaere/debridav/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version=v0.2.0 # fixed for now
 printf "%s" "${version}"
