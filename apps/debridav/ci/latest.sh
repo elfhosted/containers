dc4a57f036d189721a860905dd95bc13168e0618
David Young <davidy@funkypenguin.co.nz>
2025-01-21 23:34:01 +1300

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/debridav/ci/latest.sh
+++ apps/debridav/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET https://api.github.com/repos/skjaere/debridav/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version=v0.2.0 # fixed for now
+# version=v0.2.0 # fixed for now
 printf "%s" "${version}"
