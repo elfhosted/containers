6c71c76421a4e2bb933f03bb18bac52ba83cd6e1
David Young <davidy@funkypenguin.co.nz>
2025-01-24 21:14:43 +1300

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/arrtools/arrtools.sh
+++ apps/arrtools/arrtools.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-# Start lshell in the background
-# readonly PATH=$HOME/programs
-
-export HOME=/home/elfie
-export PATH=$PATH:$HOME/.local/bin/
-
-cd /storage
-tmux -f /restricted.tmux.conf new-session -x 80 -y 24 -A -d -s arrtools fish
-
-# Prepare a tmux entry to the already-running process, use solarized dark colors
-ttyd -p 3001 -W \
- -t titleFixed='Arrtools | ElfHosted' \
- -t 'theme={"background": "#1c1c1c", "brightGreen": "#585858", "blue": "#0087ff", "black": "#262626", "brightBlack": "#1c1c1c", "brightBlue": "#808080", "brightCyan": "#8a8a8a", "brightMagenta": "#5f5faf", "brightRed": "#d75f00", "brightWhite": "#ffffd7", "brightYellow": "#626262", "cyan": "#00afaf", "green": "#5f8700", "magenta": "#af005f", "red": "#d70000", "white": "#e4e4e4", "yellow": "#af8700"}' \
- -t drawBoldTextInBrightColors=false \
- /launch-tmux.sh
+# Launch fish shell
+/bin/fish
