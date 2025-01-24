91deed66d4d490a819aaf1882272c60e3c30f5d3
David Young <davidy@funkypenguin.co.nz>
2025-01-24 19:58:28 +1300

    upadte
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/arrtools/ci/latest.sh
+++ apps/arrtools/ci/latest.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/sureshfizzy/CineSync/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*CineSync-}"
+version=$(curl -sX GET "https://api.github.com/sirk123au/ArrTools/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 printf "%s" "${version}"    
+
+
 
