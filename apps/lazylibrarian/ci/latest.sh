3d319eefb21c03acee32828a8e5a22b3d9558448
David Young <davidy@funkypenguin.co.nz>
2025-02-04 17:50:02 +1300

    tidyup
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/lazylibrarian/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version="$(curl -sX GET "https://gitlab.com/api/v4/projects/9317860/repository/commits/master" | awk '/id/{print $4;exit}' FS='[""]')"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
