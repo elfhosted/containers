86b0eaa0c8b35f212ac8d893d6d4271c60b7da39
David Young <davidy@funkypenguin.co.nz>
2025-02-04 17:19:26 +1300

    tidy up
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/zyclops/ci/latest.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/elfhosted/zyclops/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-printf "%s" "${version}"
