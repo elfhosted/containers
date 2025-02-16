815eb0846e1e07466fb65c103dbe9047fc1a88a5
David Young <davidy@funkypenguin.co.nz>
2025-02-17 09:58:16 +1300

    Update gotosocial

---
+++ apps/gotosocial/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start riven in the background
+tmux -f /restricted.tmux.conf new-session -x 80 -y 24 -A -d -s gotosocial /gotosocial.sh
+
+# Prepare a tmux entry to the already-running process, use solarized dark colors
+ttyd -p 3001 -W \
+ -t titleFixed='GoToSocial | ElfHosted' \
+ -t 'theme={"background": "#1c1c1c", "brightGreen": "#585858", "blue": "#0087ff", "black": "#262626", "brightBlack": "#1c1c1c", "brightBlue": "#808080", "brightCyan": "#8a8a8a", "brightMagenta": "#5f5faf", "brightRed": "#d75f00", "brightWhite": "#ffffd7", "brightYellow": "#626262", "cyan": "#00afaf", "green": "#5f8700", "magenta": "#af005f", "red": "#d70000", "white": "#e4e4e4", "yellow": "#af8700"}' \
+ -t drawBoldTextInBrightColors=false \
+ /launch-tmux.sh
---
+++ apps/gotosocial/root/home/elfie/.config/fish/config.fish
@@ -0,0 +1,2 @@
+set -U fish_greeting "Welcome to the ElfHosted GoToSocial CLI! Run \"gotosocial\", and for help, see https://elfhosted.com/app/gotosocial/"
+fish_add_path /gotosocial
