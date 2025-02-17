aa501f74db49bdc9bb94cc516ab465e7414ab323
David Young <davidy@funkypenguin.co.nz>
2025-02-17 22:03:26 +1300

    work on PDS
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/bluesky-pds/ci/latest.sh
+++ apps/bluesky-pds/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/bluesky-social/pds/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version=$(curl -s "https://api.github.com/repos/bluesky-social/pds/tags" --header "Authorization: Bearer ${TOKEN}"  | jq -r '[.[] | .name | select(test("^v?[0-9]+(\\.[0-9]+)*(\\.[0-9]+)*$"))] | sort_by(sub("^v"; "") | split(".") | map(try tonumber catch 0)) | reverse[0]')
 version="${version#*v}"
 printf "%s" "${version}"
