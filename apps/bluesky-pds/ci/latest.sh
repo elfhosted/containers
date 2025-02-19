698f979c7e351a3cd402129d1d170f787082c367
David Young <davidy@funkypenguin.co.nz>
2025-02-19 14:19:50 +1300

    update for PDS release mechanism

--- apps/bluesky-pds/ci/latest.sh
+++ apps/bluesky-pds/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-version=$(curl -s "https://api.github.com/repos/bluesky-social/pds/tags" --header "Authorization: Bearer ${TOKEN}"  | jq -r '[.[] | .name | select(test("^v?[0-9]+(\\.[0-9]+)*(\\.[0-9]+)*$"))] | sort_by(sub("^v"; "") | split(".") | map(try tonumber catch 0)) | reverse[0]')
-version="${version#*v}"
+version=$(curl -s "https://api.github.com/repos/bluesky-social/atproto/releases" --header "Authorization: Bearer ${TOKEN}"  | jq -r '[.[] | select((.name // "" | contains("pds")) or (.tag_name | contains("pds")))][0].tag_name')
+version="${version#*@atproto/pds@}"
 printf "%s" "${version}"
