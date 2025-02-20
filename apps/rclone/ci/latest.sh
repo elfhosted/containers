ca1f346b88f018e29def2a0fa22fb84892db9583
David Young <davidy@funkypenguin.co.nz>
2025-02-20 23:22:01 +1300

    prevent duplicate rclones
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rclone/ci/latest.sh
+++ apps/rclone/ci/latest.sh
@@ -5,7 +5,7 @@ version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=rclone&inre
 
 # If we get no result from upstream, just fake it
 if [[ "${version}" == "" ]]; then
-    version=1.69
+    version=1.69.0
 fi
 printf "%s" "${version}"
 # printf "%s" "1.66.0" # the above link 403s currently
