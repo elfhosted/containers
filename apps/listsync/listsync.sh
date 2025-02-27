cf5a2c980f3008f19abf2f106959b7b9e6464126
David Young <davidy@funkypenguin.co.nz>
2025-02-28 12:02:38 +1300

    update listsync entrypoint
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/listsync/listsync.sh
+++ apps/listsync/listsync.sh
@@ -4,14 +4,29 @@ if [[ -z "$OVERSEERR_API_KEY" ]];
 then
     echo "ListSync uses populates Overseerr / Jellyseerr with requests from IMDB, Trakt, or Letterboxd lists
 
-Before ListSync will run, you need to define:
+Before ListSync will run, you need to define at least:
 - OVERSEERR_API_KEY
 
 See https://docs.elfhosted.com/app/listsync for further details"
     sleep infinity 
 fi
 
-python add.py
 
-echo "ListSync has unexpectedly exited :( Press any key to restart, or wait 5 min... (incase you need to capture debug output)"
+#!/bin/bash
+
+echo "ListSync usually runs in automated (hands-off) mode. 
+Press any key to drop to a shell for manual mode, or wait 10 seconds for a normal start..."
+
+# -t 10: Timeout of 10 seconds
+read -s -n 1 -t 10
+
+if [ $? -eq 0 ]; then
+    echo "You pressed a key! Going to manual mode.."
+    python add.py
+else
+    echo "Timeout reached, running ListSync in automated mode..
+    AUTOMATED_MODE=true python app.py
+fi
+
+echo "ListSync has exited :( Press any key to restart, or wait 5 min... (incase you need to capture debug output)"
 read -s -n 1 -t 300
