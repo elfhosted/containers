c70944058692f5d99df8ee51a06482ca44528695
David Young <davidy@funkypenguin.co.nz>
2025-02-28 08:47:23 +1300

    bump

--- apps/listsync/ci/latest.sh
+++ apps/listsync/ci/latest.sh
@@ -2,8 +2,8 @@
 channel=$1
 
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/Woahai321/ListSync/commits/dev" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/Woahai321/list-sync/commits/dev" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
-    version="$(curl -sX GET "https://api.github.com/repos/Woahai321/ListSync/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
+    version="$(curl -sX GET "https://api.github.com/repos/Woahai321/list-sync/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
 fi
 printf "%s" "${version}"
