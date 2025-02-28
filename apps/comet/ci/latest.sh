1d57b7f9451f91054191c8d6f6515b653ff77223
David Young <davidy@funkypenguin.co.nz>
2025-03-01 10:31:40 +1300

    tidy up comet build
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/comet/ci/latest.sh
+++ apps/comet/ci/latest.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-channel=$1
-
-if [[ "${channel}" == "rewrite" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/g0ldyy/comet/commits/rewrite" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-else
-    version=$(curl -sX GET "https://api.github.com/repos/g0ldyy/comet/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-fi
+version=$(curl -sX GET "https://api.github.com/repos/g0ldyy/comet/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"
