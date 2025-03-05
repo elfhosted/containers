90c2e6b3b92b8db55013cd75ca9f6156b755afe8
David Young <davidy@funkypenguin.co.nz>
2025-03-05 14:07:28 +1300

    add helper scripts for plex-request bits
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/wests-blackhole-script/request.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -z "${OVERSEERR_HOST}" ]]; then
+  echo "OVERSEERR_HOST env var is not set, sleeping..."
+  sleep infinity
+fi
+
+cd /app
+gunicorn --bind 0.0.0.0:32501 plex_request_wsgi:app
