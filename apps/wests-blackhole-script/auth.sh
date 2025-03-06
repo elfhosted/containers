7798ae09cac3d7344c518dbf5a70aff1d3492df5
David Young <davidy@funkypenguin.co.nz>
2025-03-06 17:21:55 +1300

    workaround tokens.json issue
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/wests-blackhole-script/auth.sh
+++ apps/wests-blackhole-script/auth.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Create a dummy tokens.json if it doesn't exist
+if [ ! -f /config/tokens.json ]; then
+    echo "[]" > /config/tokens.json
+fi
+
 if [[ -z "${OVERSEERR_HOST}" ]]; then
   echo "OVERSEERR_HOST env var is not set, sleeping..."
   sleep infinity
