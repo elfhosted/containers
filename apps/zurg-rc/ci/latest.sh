10fc0c060cbda7b6362dd4831fa26aeeeec43cb2
David Young <davidy@funkypenguin.co.nz>
2025-03-16 18:18:40 +1300

    build dev version of zurg
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/zurg-rc/ci/latest.sh
+++ apps/zurg-rc/ci/latest.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 channel=$1
-version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg/releases --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | first | .tag_name')
-printf "%s" "${version}"
 
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/debridmediamanager/zurg/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg/releases --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | first | .tag_name')
+fi
+
+printf "%s" "${version}"
