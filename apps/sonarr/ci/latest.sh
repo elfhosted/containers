550dea49be02d9aca77c6d785d258d052389f583
David Young <davidy@funkypenguin.co.nz>
2025-03-18 08:30:36 +1300

    fix channel

--- apps/sonarr/ci/latest.sh
+++ apps/sonarr/ci/latest.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+channel=$1
 version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=4.0" | jq --raw-output '.version')
 version="${version#*v}"
 version="${version#*release-}"
