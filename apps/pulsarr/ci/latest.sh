e29c624295ea328741ef24957f83d052b0a8a515
David Young <davidy@funkypenguin.co.nz>
2025-03-19 08:57:42 +1300

    add devel build for pulsarr
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/pulsarr/ci/latest.sh
+++ apps/pulsarr/ci/latest.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET https://api.github.com/repos/jamcalli/pulsarr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+channel=$1
+if [[ "${channel}" == "devel" ]]; then
+    version=$(curl -sX GET https://api.github.com/repos/jamcalli/pulsarr/commits/devel --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/jamcalli/pulsarr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
 printf "%s" "${version}"
