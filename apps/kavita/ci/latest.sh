ebb966572f5f2d387ea5b36a12b2c4d4e8ed75bb
David Young <davidy@funkypenguin.co.nz>
2025-03-22 19:30:01 +1300

    update kavita
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/kavita/ci/latest.sh
+++ apps/kavita/ci/latest.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/Kareadita/Kavita/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*v}"
-version="${version#*release-}"
+version=$(curl -s "https://hub.docker.com/v2/repositories/jvmilazz0/kavita/tags?page_size=100" | jq -r '.results[0].name')
 printf "%s" "${version}"
