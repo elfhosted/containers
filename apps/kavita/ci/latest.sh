cd5f3c28b115e50c7ce5ef7c2881d7347438366d
David Young <davidy@funkypenguin.co.nz>
2025-03-22 19:38:15 +1300

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/kavita/ci/latest.sh
+++ apps/kavita/ci/latest.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -s "https://hub.docker.com/v2/repositories/jvmilazz0/kavita/tags?page_size=100" | jq -r '.results[0].name')
+version=$(curl -s "https://hub.docker.com/v2/repositories/jvmilazz0/kavita/tags?page_size=100" | 
+          jq -r '.results[] | select(.name | test("^[0-9]+\\.[0-9]+\\.[0-9]+$")) | .name' | 
+          head -n 1)
 printf "%s" "${version}"
