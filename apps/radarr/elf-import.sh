bb65e0d2fd8e21f916ce453fd3c6cba0c2fd8807
David Young <davidy@funkypenguin.co.nz>
2025-03-26 09:36:31 +1300

    update import script
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/radarr/elf-import.sh
+++ apps/radarr/elf-import.sh
@@ -7,14 +7,15 @@ if echo "$radarr_sourcepath" | grep -q "/storage/symlinks"; then
 else
     # Get the parent directory of radarr_sourcepath
     parent_dir=$(dirname "$radarr_sourcepath")
+    grandparent_dir=$(dirname "$parent_dir")
     
-    # Create the "symlinked" directory in the parent directory
-    symlinked_dir="$parent_dir/symlinked"
+    # Create the "symlinked" directory in the grandparent directory
+    symlinked_dir="$grandparent_dir/symlinked"
     mkdir -p "$symlinked_dir"
     
-    # Move the source file to the new directory (so that the aarr can't delete it)
-    mv "$radarr_sourcepath" "$symlinked_dir/"
+    # Move the parent dir file to the new grandparent directory (so that the aarr can't delete it)
+    mv "$parent_dir" "$symlinked_dir/"
 
     # Create a symlink at radarr_destinationpath pointing to the copied file
-    ln -s "$symlinked_dir/$(basename "$radarr_sourcepath")" "$radarr_destinationpath"
+    ln -s "$symlinked_dir/$(basename "$parent_dir")/$(basename "$radarr_sourcepath")" "$radarr_destinationpath"
 fi
--- apps/sonarr/elf-import.sh
+++ apps/sonarr/elf-import.sh
@@ -7,14 +7,15 @@ if echo "$sonarr_sourcepath" | grep -q "/storage/symlinks"; then
 else
     # Get the parent directory of sonarr_sourcepath
     parent_dir=$(dirname "$sonarr_sourcepath")
+    grandparent_dir=$(dirname "$parent_dir")
     
-    # Create the "symlinked" directory in the parent directory
-    symlinked_dir="$parent_dir/symlinked"
+    # Create the "symlinked" directory in the grandparent directory
+    symlinked_dir="$grandparent_dir/symlinked"
     mkdir -p "$symlinked_dir"
     
-    # Move the source file to the new directory (so that the aarr can't delete it)
-    mv "$sonarr_sourcepath" "$symlinked_dir/"
+    # Move the parent dir file to the new grandparent directory (so that the aarr can't delete it)
+    mv "$parent_dir" "$symlinked_dir/"
 
     # Create a symlink at sonarr_destinationpath pointing to the copied file
-    ln -s "$symlinked_dir/$(basename "$sonarr_sourcepath")" "$sonarr_destinationpath"
+    ln -s "$symlinked_dir/$(basename "$parent_dir")/$(basename "$sonarr_sourcepath")" "$sonarr_destinationpath"
 fi
