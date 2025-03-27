81ccc71acd440879ea4309e8460dc2f1b9b74290
David Young <davidy@funkypenguin.co.nz>
2025-03-28 08:30:40 +1300

    update for pulsarr develop
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/pulsarr/ci/latest.sh
+++ apps/pulsarr/ci/latest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 channel=$1
-if [[ "${channel}" == "devel" ]]; then
-    version=$(curl -sX GET https://api.github.com/repos/jamcalli/pulsarr/commits/devel --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+if [[ "${channel}" == "develop" ]]; then
+    version=$(curl -sX GET https://api.github.com/repos/jamcalli/pulsarr/commits/develop --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/jamcalli/pulsarr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
