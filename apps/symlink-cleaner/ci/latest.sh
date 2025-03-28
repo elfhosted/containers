1f12679a368aa4aab2adeb3c67296bceb83f8cfa
David Young <davidy@funkypenguin.co.nz>
2025-03-29 04:51:20 +1300

    add versioning to symlink-cleaner
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/symlink-cleaner/ci/latest.sh
+++ apps/symlink-cleaner/ci/latest.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/bsm-elf/symlink-cleaner/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-printf "%s" "${version}"    
 
+channel=$1
 
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/bsm-elf/symlink-cleaner/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/bsm-elf/symlink-cleaner/releases --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | first | .tag_name')
+fi
 
+printf "%s" "${version}"
