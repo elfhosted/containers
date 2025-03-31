24bcec6966b41fb4d9309f654b7e32b698a04910
David Young <davidy@funkypenguin.co.nz>
2025-04-01 12:00:39 +1300

    update shim

--- apps/ffprobe-shim/ci/latest.sh
+++ apps/ffprobe-shim/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/elfhosted/ffprobe-shim/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET "https://api.github.com/repos/elfhosted/ffprobe-shim/commits/main" --header "Authorization: Bearer ${$ZURG_GH_CREDS}" | jq --raw-output '.sha')
 printf "%s" "${version}"    
 
 
