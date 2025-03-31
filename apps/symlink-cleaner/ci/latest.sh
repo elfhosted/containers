b37684a52163397118b0b5ded33f2776996e98cf
David Young <davidy@funkypenguin.co.nz>
2025-04-01 07:57:31 +1300

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/symlink-cleaner/ci/latest.sh
+++ apps/symlink-cleaner/ci/latest.sh
@@ -3,7 +3,7 @@
 channel=$1
 
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/bsm-elf/symlink-cleaner/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/bsm-elf/symlink-cleaner/commits/main" --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/bsm-elf/symlink-cleaner/releases --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | first | .tag_name')
 fi
