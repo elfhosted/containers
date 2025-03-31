a310ccd20cb85e87149c999e98e2e89e2a5d7966
David Young <davidy@funkypenguin.co.nz>
2025-04-01 10:06:34 +1300

    update apprise

--- apps/rclone/rootfs/debrid-provider.sh
+++ apps/rclone/rootfs/debrid-provider.sh
@@ -6,12 +6,14 @@
     --max-header-bytes=8192 \
     --no-modtime \
     --vfs-read-chunk-size=10M \
-    --vfs-read-chunk-size-limit=10M \
-    --dir-cache-time 10s \
-    --multi-thread-streams=0 \
-    --cutoff-mode=cautious \
-    --vfs-cache-mode writes \
-    --buffer-size=0 \
-    -v \
+    --vfs-read-chunk-size-limit=100M \
+    --vfs-cache-max-age=1h \
+    --dir-cache-time 60s \
+    --multi-thread-streams=4 \
+    --cutoff-mode=soft \
+    --vfs-cache-mode full \
+    --buffer-size=32M \
+    --poll-interval=0 \
+    -vv \
     debrid-provider:
 
