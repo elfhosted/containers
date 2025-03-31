091b655c91ad464c9def0a77a2fb3ad05284bdfb
David Young <davidy@funkypenguin.co.nz>
2025-04-01 12:31:28 +1300

    update

--- apps/radarr/ci/latest.sh
+++ apps/radarr/ci/latest.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 channel=$1
-version=$(curl -sX GET "https://radarr.servarr.com/v1/update/${channel}/changes?os=linux&runtime=netcore" | jq --raw-output '.[0].version' 2>/dev/null)
+
+if [[ "${channel}" == "dev" ]]; then
+    # fake it
+    version=$(curl -sX GET "https://radarr.servarr.com/v1/update/main/changes?os=linux&runtime=netcore" | jq --raw-output '.[0].version' 2>/dev/null)
+else
+    version=$(curl -sX GET "https://radarr.servarr.com/v1/update/${channel}/changes?os=linux&runtime=netcore" | jq --raw-output '.[0].version' 2>/dev/null)
+fi
+
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
