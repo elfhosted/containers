df8cfcbea4cebf8ee18d884419bd2127b48cc686
David Young <davidy@funkypenguin.co.nz>
2025-04-04 07:56:44 +1300

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/sonarr/ci/latest.sh
+++ apps/sonarr/ci/latest.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 channel=$1
-version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=4.0" | jq --raw-output '.version')
+
+if [[ "${channel}" == "dev" ]]; then
+    # fake it
+    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/master?version=4.0" | jq --raw-output '.version')
+else
+    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=4.0" | jq --raw-output '.version')
+fi
+
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
