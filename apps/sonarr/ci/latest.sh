fa530a083ec686608e49d275f5b29ef238a158fb
David Young <davidy@funkypenguin.co.nz>
2025-04-04 08:43:14 +1300

    bump sonarr
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/sonarr/ci/latest.sh
+++ apps/sonarr/ci/latest.sh
@@ -3,7 +3,7 @@ channel=$1
 
 if [[ "${channel}" == "dev" ]]; then
     # fake it
-    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/master?version=4.0" | jq --raw-output '.version')
+    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/main?version=4.0" | jq --raw-output '.version')
 else
     version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=4.0" | jq --raw-output '.version')
 fi
