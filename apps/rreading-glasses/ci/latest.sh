03a4dbd92cb481d20f9effac171bcf8cec05abb6
David Young <davidy@funkypenguin.co.nz>
2025-04-25 20:30:26 +1200

    update rreading-glasses
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/rreading-glasses/ci/latest.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET "https://api.github.com/repos/blampe/rreading-glasses/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+printf "%s" "${version}"
