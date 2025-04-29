ff54949e0a6f0c6d72a834911ebc798fa099ccb3
David Young <davidy@funkypenguin.co.nz>
2025-04-29 23:41:49 +1200

    build beta of decypharr
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/decypharr/ci/latest.sh
+++ apps/decypharr/ci/latest.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/sirrobot01/debrid-blackhole/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*release-}"
+
+channel=$1
+
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/sirrobot01/debrid-blackhole/commits/beta" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/sirrobot01/debrid-blackhole/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
 printf "%s" "${version}"
