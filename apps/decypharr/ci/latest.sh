ced2f6e91388eeb379bbdea73f5f519b62b8809f
David Young <davidy@funkypenguin.co.nz>
2025-05-23 16:08:39 +1200

    Update link to decypharr
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/decypharr/ci/latest.sh
+++ apps/decypharr/ci/latest.sh
@@ -3,8 +3,8 @@
 channel=$1
 
 if [[ "${channel}" == "beta" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/sirrobot01/debrid-blackhole/commits/beta" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/sirrobot01/decypharr/commits/beta" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
-    version=$(curl -sX GET https://api.github.com/repos/sirrobot01/debrid-blackhole/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+    version=$(curl -sX GET https://api.github.com/repos/sirrobot01/decypharr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
 printf "%s" "${version}"
