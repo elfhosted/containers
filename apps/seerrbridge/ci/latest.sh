de6eed4dc9c11551ce5584ffecc4f8c23dcdf55e
David Young <davidy@funkypenguin.co.nz>
2025-05-25 09:52:15 +1200

    update seerrbridge
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/seerrbridge/ci/latest.sh
+++ apps/seerrbridge/ci/latest.sh
@@ -2,7 +2,7 @@
 channel=$1
 
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/Woahai321/SeerrBridge/commits/dev" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/Woahai321/SeerrBridge/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version="$(curl -sX GET "https://api.github.com/repos/Woahai321/SeerrBridge/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
 fi
