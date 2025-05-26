d7cebed18e61dafbad313fbdf6ce5016a10a860a
David Young <davidy@funkypenguin.co.nz>
2025-05-26 19:21:43 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/peertube/ci/latest.sh
+++ apps/peertube/ci/latest.sh
@@ -1,3 +1,7 @@
+# #!/usr/bin/env bash
+# version=$(curl -s -S "https://registry.hub.docker.com/v2/repositories/chocobozzz/peertube/tags/" | jq -r '."results"[]["name"]' | grep -vE "develop|latest" | head -1)
+# printf "%s" "${version}"
+
 #!/usr/bin/env bash
-version=$(curl -s -S "https://registry.hub.docker.com/v2/repositories/chocobozzz/peertube/tags/" | jq -r '."results"[]["name"]' | grep -vE "develop|latest" | head -1)
+version=$(curl -sX GET https://api.github.com/repos/Chocobozzz/PeerTube/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"
