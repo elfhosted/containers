e321570424b6e1c5ff153dc94e14240b10baf063
David Young <davidy@funkypenguin.co.nz>
2025-05-31 11:27:47 +1200

    add dev version of aiolists
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/aiolists/ci/latest.sh
+++ apps/aiolists/ci/latest.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/SebastianMorel/AIOLists/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*CineSync-}"
-printf "%s" "${version}"    
 
+channel=$1
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/SebastianMorel/AIOLists/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/SebastianMorel/AIOLists/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
+printf "%s" "${version}"
