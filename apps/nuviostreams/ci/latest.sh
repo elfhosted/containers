ac2f3e48bb6786785dcae479e1dc8d7629c75428
David Young <davidy@funkypenguin.co.nz>
2025-06-03 09:30:17 +1200

    Add nuviostreams
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/nuviostreams/ci/latest.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+channel=$1
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/tapframe/NuvioStreamsAddon/commits/master" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/tapframe/NuvioStreamsAddon/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
+printf "%s" "${version}"
