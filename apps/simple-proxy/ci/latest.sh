ceb5ff768b7b342a8b67c77ab242f16e56490f9b
David Young <davidy@funkypenguin.co.nz>
2025-06-07 20:29:11 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/simple-proxy/ci/latest.sh
+++ apps/simple-proxy/ci/latest.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
 channel=$1
-if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/SebastianMorel/AIOLists/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-else
-    version=$(curl -sX GET https://api.github.com/repos/SebastianMorel/AIOLists/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-fi
+version=$(curl -sX GET "https://api.github.com/repos/p-stream/simple-proxy/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 printf "%s" "${version}"
