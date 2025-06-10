dd2184294ca98405e9b7146079a220f98ca41b71
David Young <davidy@funkypenguin.co.nz>
2025-06-11 11:59:09 +1200

    add csi-rclone
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rapidleech/ci/latest.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/SSoft7/rapidleech/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*release-}"
-printf "%s" "${version}"
