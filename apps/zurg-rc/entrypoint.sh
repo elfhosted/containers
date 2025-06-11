c66180d0df87eccef3fb48278618b3cbd492faa5
David Young <davidy@funkypenguin.co.nz>
2025-06-11 14:32:59 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/zurg-rc/entrypoint.sh
+++ apps/zurg-rc/entrypoint.sh
@@ -17,9 +17,8 @@ if [[ "${WAIT_FOR_WARP:-"false"}" == "true" ]]; then
 fi
 
 if [[ "${DECYPHARR_REPLACE_ZURG:-"false"}" == "true" ]]; then
-    echo "Zurg is replaced by decypharr, doing nothing.."
     while true; do
-        socat TCP-LISTEN:9999,reuseaddr,fork SYSTEM:'echo -e "HTTP/1.1 200 OK\r\nContent-Length: 44\r\n\r\nZurg is replaced by decypharr, doing nothing!"'
+        socat TCP-LISTEN:9999,reuseaddr,fork SYSTEM:"printf 'HTTP/1.1 200 OK\r\nContent-Length: 44\r\n\r\nZurg is replaced by decypharr, doing nothing!'"
     done
 else
     cd /config
