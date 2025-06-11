cc747b713752adfb30c94f17b78802e251c8691c
David Young <davidy@funkypenguin.co.nz>
2025-06-11 12:38:37 +1200

    add dev version of plexytrack
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plexytrack/ci/latest.sh
+++ apps/plexytrack/ci/latest.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/Drakonis96/plexytrack/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
+
+channel=$1
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/Drakonis96/plexytrack/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/Drakonis96/plexytrack/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
 printf "%s" "${version}"
