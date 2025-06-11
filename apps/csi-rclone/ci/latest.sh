0d0f12bbad09f13745ea804629a6f99279a72ad3
David Young <davidy@funkypenguin.co.nz>
2025-06-11 14:13:35 +1200

    build updated csi-rclone image
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/csi-rclone/ci/latest.sh
+++ apps/csi-rclone/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/dvcrn/csi-rclone-reloaded/commits/master" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET "https://api.github.com/repos/ceresimaging/csi-rclone/commits/master" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 version="${version#*release-}"
 printf "%s" "${version}"
