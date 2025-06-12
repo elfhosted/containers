5539ee682d1052af5c0ca3e5b2818fa3cf5bb24d
David Young <davidy@funkypenguin.co.nz>
2025-06-13 09:14:11 +1200

    build plex beta release
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/plex/ci/latest.sh
+++ apps/plex/ci/latest.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 channel=$1
 
-if [[ "${channel}" == "beta" ]]; then
-    git clone --quiet --depth=1 https://aur.archlinux.org/plex-media-server-plexpass.git /tmp/plex-media-server-plexpass
-    pushd /tmp/plex-media-server-plexpass > /dev/null || exit
-    version="$(grep -oP "(?<=pkgver=).*" PKGBUILD)"
-    version="${version}-$(grep -oP "(?<=_pkgsum=).*" PKGBUILD)"
-    popd > /dev/null || exit
-    rm -rf /tmp/plex-media-server-plexpass
-fi
+json=$(curl -sX GET 'https://plex.tv/api/downloads/5.json')
 
 if [[ "${channel}" == "stable" ]]; then
-    version=$(curl -sX GET 'https://plex.tv/api/downloads/5.json' | jq -r '.computer.Linux.version')
+    version=$(echo "$json" | jq -r '.computer.Linux.releases[] | select(.channel == "public") | .version' | head -n1)
+elif [[ "${channel}" == "beta" ]]; then
+    version=$(echo "$json" | jq -r '.computer.Linux.releases[] | select(.channel == "plexpass") | .version' | head -n1)
 fi
 
 version="${version#*v}"
