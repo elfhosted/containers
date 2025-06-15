2a8c195431d88886e26d0044c5ab9b3d777704a8
David Young <davidy@funkypenguin.co.nz>
2025-06-16 11:54:51 +1200

    try building latest list-sync
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/listsync/ci/latest.sh
+++ apps/listsync/ci/latest.sh
@@ -2,7 +2,7 @@
 channel=$1
 
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/Woahai321/list-sync/commits/dev" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/Woahai321/list-sync/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version="$(curl -sX GET "https://api.github.com/repos/Woahai321/list-sync/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
 fi
