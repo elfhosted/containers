6e1c91a4b0a7244c6b04a1d31b44dc1774be357b
David Young <davidy@funkypenguin.co.nz>
2025-06-16 22:51:33 +1200

    bump
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/csi-rclone/ci/latest.sh
+++ apps/csi-rclone/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=rclone&inrepo=alpine_edge" -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" | jq -r '.rclone | .[] | select((.repo == "alpine_edge" and .binname == "rclone")) | .version')
+# version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=rclone&inrepo=alpine_edge" -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" | jq -r '.rclone | .[] | select((.repo == "alpine_edge" and .binname == "rclone")) | .version')
 # version="${version%%_*}"
 # version="${version%%-*}"
 
