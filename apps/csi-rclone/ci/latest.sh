64e9d6b4378e3343e9e627ae703bd7f178674930
David Young <davidy@funkypenguin.co.nz>
2025-06-16 14:22:39 +1200

    use a decent version for csi-rclone
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/csi-rclone/ci/latest.sh
+++ apps/csi-rclone/ci/latest.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/ceresimaging/csi-rclone/commits/master" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-version="${version#*release-}"
+version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=rclone&inrepo=alpine_edge" -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" | jq -r '.rclone | .[] | select((.repo == "alpine_edge" and .binname == "rclone")) | .version')
+# version="${version%%_*}"
+# version="${version%%-*}"
+
+# If we get no result from upstream, just fake it
+if [[ "${version}" == "" ]]; then
+    version=1.69.3
+fi
 printf "%s" "${version}"
+# printf "%s" "1.66.0" # the above link 403s currently
