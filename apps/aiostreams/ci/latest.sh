6b6352e06e40c260f2d0739938eaf1bc9fb39651
David Young <davidy@funkypenguin.co.nz>
2025-06-19 10:31:17 +1200

    fix aiostreams dev
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/aiostreams/ci/latest.sh
+++ apps/aiostreams/ci/latest.sh
@@ -2,7 +2,7 @@
 
 channel=$1
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/Viren070/AIOStreams/commits/rewrite" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/Viren070/AIOStreams/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/Viren070/AIOStreams/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
