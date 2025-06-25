d581b89b03d62ce82ed0dc51034b6c2c2ca048ed
David Young <davidy@funkypenguin.co.nz>
2025-06-26 09:35:06 +1200

    update wger
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/wger/ci/latest.sh
+++ apps/wger/ci/latest.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-channel=$1
-
-if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/westsurname/scripts/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-else
-    version=$(curl -sX GET https://api.github.com/repos/westsurname/scripts/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-fi
-
-version="${version#*release-}"
+version=$(curl -sX GET https://api.github.com/repos/wger-project/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"
