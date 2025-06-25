c2d202ca34590771aee41dba47db5be1dccc015d
David Young <davidy@funkypenguin.co.nz>
2025-06-26 06:25:37 +1200

    first cut at wger
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/wests-blackhole-script/request.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -z "${OVERSEERR_HOST}" ]]; then
-  echo "OVERSEERR_HOST env var is not set, sleeping..."
-  sleep infinity
-fi
-
-cd /app
-gunicorn --bind 0.0.0.0:32502 plex_request_wsgi:app
--- apps/wests-blackhole-script/watchlist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -z "${OVERSEERR_HOST}" ]]; then
-  echo "OVERSEERR_HOST env var is not set, sleeping..."
-  sleep infinity
-fi
-
-cd /app
-python watchlist_runner.py
