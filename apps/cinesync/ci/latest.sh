937175ec0d9ab5ce169ea92eea60239c262c66a4
David Young <davidy@funkypenguin.co.nz>
2025-06-26 23:54:09 +1200

    fix latest
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/cinesync/ci/latest.sh
+++ apps/cinesync/ci/latest.sh
@@ -2,7 +2,7 @@
 
 channel=$1
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/sureshfizzy/CineSync/commits/featur" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/sureshfizzy/CineSync/commits/feature" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/sureshfizzy/CineSync/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
