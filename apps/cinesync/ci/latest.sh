ec4d695344d84a0be0525390ef7fd38fd3ae8f93
David Young <davidy@funkypenguin.co.nz>
2025-06-26 23:30:38 +1200

    update cinesync
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/cinesync/ci/latest.sh
+++ apps/cinesync/ci/latest.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/sureshfizzy/CineSync/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*CineSync-}"
-printf "%s" "${version}"    
 
+channel=$1
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/sureshfizzy/CineSync/commits/featur" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/sureshfizzy/CineSync/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
+printf "%s" "${version}"
