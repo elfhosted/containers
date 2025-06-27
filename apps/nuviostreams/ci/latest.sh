f1eda6246991872f3b69526a59fcc7e75b1d7ccc
David Young <davidy@funkypenguin.co.nz>
2025-06-27 19:04:38 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/nuviostreams/ci/latest.sh
+++ apps/nuviostreams/ci/latest.sh
@@ -3,7 +3,7 @@
 channel=$1
 if [[ "${channel}" == "dev" ]]; then
     version=$(curl -sX GET "https://api.github.com/repos/tapframe/NuvioStreamsAddon/commits/master" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-else if [[ "${channel}" == "private" ]]; then
+elif [[ "${channel}" == "private" ]]; then
     version=$(curl -sX GET "https://api.github.com/repos/tapframe/NuvioStreamsAddon/commits/private" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/tapframe/NuvioStreamsAddon/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
