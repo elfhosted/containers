0fbf20f1655d0092a09e2bf63d86edb3993ca53e
David Young <davidy@funkypenguin.co.nz>
2025-07-03 16:54:08 +1200

    try 2.4
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/wger/ci/latest.sh
+++ apps/wger/ci/latest.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 version=$(curl -sX GET https://api.github.com/repos/wger-project/wger/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-printf "%s" "${version}"
+
+# printf "%s" "${version}"
+printf %s "2.4"
