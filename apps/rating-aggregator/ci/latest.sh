bde3bfd6c51264832976fb17e2cfee0e4d5f1540
David Young <davidy@funkypenguin.co.nz>
2025-07-09 09:43:42 +1200

    update rating-aggregator
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/rating-aggregator/ci/latest.sh
+++ apps/rating-aggregator/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos//anmol210202/rating-aggregator-/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET https://api.github.com/repos/anmol210202/rating-aggregator-/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"
