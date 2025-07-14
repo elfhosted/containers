db92b0f5d6799b78a9053e2c16adbab51144e7e4
David Young <davidy@funkypenguin.co.nz>
2025-07-15 09:10:59 +1200

    switch to develop branch

--- apps/elfbot/ci/latest.sh
+++ apps/elfbot/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/jamcalli/elfbot/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET "https://api.github.com/repos/jamcalli/elfbot/commits/develop" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 printf "%s" "${version}"
