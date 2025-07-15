a80cc528b1d1e42a665b09a23f7682a2ff72028a
David Young <davidy@funkypenguin.co.nz>
2025-07-15 16:15:04 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/elfbot/ci/latest.sh
+++ apps/elfbot/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/jamcalli/elfbot/commits/develop" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET "https://api.github.com/repos/jamcalli/elfbot/commits/develop" --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '.sha')
 printf "%s" "${version}"
