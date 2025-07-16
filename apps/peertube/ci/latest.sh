da23f9365dc5bced8f930ad6a5fd96f329ec2355
David Young <davidy@funkypenguin.co.nz>
2025-07-17 08:17:02 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/peertube/ci/latest.sh
+++ apps/peertube/ci/latest.sh
@@ -4,4 +4,4 @@
 
 #!/usr/bin/env bash
 version=$(curl -sX GET https://api.github.com/repos/Chocobozzz/PeerTube/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-printf "%s" "${version}"
+printf "%s" "${version#CineSync-}"
