beb9a2306046a1b02fa61c7de512184c95471e27
David Young <davidy@funkypenguin.co.nz>
2025-07-17 20:15:36 +1200

    bump
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/elf-discord-bot/ci/latest.sh
+++ apps/elf-discord-bot/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/jamcalli/elf-discord-bot/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET https://api.github.com/repos/funkypenguin/elf-discord-bot/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"
