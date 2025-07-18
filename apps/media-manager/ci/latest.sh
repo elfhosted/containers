bf8c54767d2b89db44ab5616cbdd1663eb639cb1
David Young <davidy@funkypenguin.co.nz>
2025-07-18 13:45:26 +1200

    Add media-manager
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/media-manager/ci/latest.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+channel=$1
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/maxdorninger/MediaManager/commits/master" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/maxdorninger/MediaManager/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
+printf "%s" "${version}"   
