b042a229fed561c2607935a33b21f58473f769df
David Young <davidy@funkypenguin.co.nz>
2025-07-22 16:05:11 +1200

    add kapowarr dev
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/kapowarr/ci/latest.sh
+++ apps/kapowarr/ci/latest.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/Casvt/Kapowarr/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
+
+channel=$1
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/asvt/Kapowarr/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/asvt/Kapowarr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
 version="${version#*V}"
-printf "%s" "${version}"
+printf "%s" "${version}"   
