714434842473f658118af4bb61493a62a5a91e19
David Young <davidy@funkypenguin.co.nz>
2025-07-22 19:45:08 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/elfbot/ci/latest.sh
+++ apps/elfbot/ci/latest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 channel=$1
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/funkypenguin/elfbot/commits/develop" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/funkypenguin/elfbot/commits/develop" --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '.sha')
 else
-    version=$(curl -sX GET https://api.github.com/repos/funkypenguin/elfbot/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+    version=$(curl -sX GET https://api.github.com/repos/funkypenguin/elfbot/releases/latest --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | .tag_name')
 fi
 printf "%s" "${version}"   
