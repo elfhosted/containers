cf9ca2b17350060c04e11d3caac0371450012b13
David Young <davidy@funkypenguin.co.nz>
2025-07-22 16:12:18 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/kapowarr/ci/latest.sh
+++ apps/kapowarr/ci/latest.sh
@@ -2,9 +2,9 @@
 
 channel=$1
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/asvt/Kapowarr/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/Casvt/Kapowarr/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
-    version=$(curl -sX GET https://api.github.com/repos/asvt/Kapowarr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+    version=$(curl -sX GET https://api.github.com/repos/Casvt/Kapowarr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
 version="${version#*V}"
 printf "%s" "${version}"   
