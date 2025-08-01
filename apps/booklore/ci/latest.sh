3b622f6521ebe77242cd1d3df9c4ec18d49c6eab
David Young <davidy@funkypenguin.co.nz>
2025-08-01 21:02:25 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/booklore/ci/latest.sh
+++ apps/booklore/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/adityachandelgit/BookLore/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version=$(curl -sX GET https://api.github.com/repos/booklore-app/BookLore/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 version="${version#*release-}"
 printf "%s" "${version}"
