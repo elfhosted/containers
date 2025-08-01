da5ed19acb1363e9fe19f20f0d048b1d0a778669
David Young <davidy@funkypenguin.co.nz>
2025-08-02 07:06:53 +1200

    remove capitalized v in cwa
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/calibre-web-automated/ci/latest.sh
+++ apps/calibre-web-automated/ci/latest.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 version="$(curl -sX GET "https://api.github.com/repos/crocodilestick/Calibre-Web-Automated/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
-version="${version#*v}"
-version="${version#*release-}"
+version="${version#*V}"
 printf "%s" "${version}"
