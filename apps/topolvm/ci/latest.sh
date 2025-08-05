e24b5d38d7ffefcf21a233f1313895faa1a0c810
David Young <davidy@funkypenguin.co.nz>
2025-08-05 13:43:43 +1200

    try again
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/topolvm/ci/latest.sh
+++ apps/topolvm/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/topolvm/topolvm/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version=$(curl -sX GET https://api.github.com/repos/topolvm/topolvm/releases | jq -r '.[].tag_name' | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n1)
 version="${version#*v}"
 printf "%s" "${version}"
