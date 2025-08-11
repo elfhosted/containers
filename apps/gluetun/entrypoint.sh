c676d6803d1b3f8a0261c39fc56f0a028f062d68
David Young <davidy@funkypenguin.co.nz>
2025-08-11 14:19:10 +1200

    allow bypassing plex VPN away from hetzner
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/gluetun/entrypoint.sh
+++ apps/gluetun/entrypoint.sh
@@ -11,11 +11,9 @@ if [[ -f /shared/env ]]; then
 fi
 
 # If we're in "sleep mode", then don't actually, just do nothing (useful when we control how a pod will run based on an env var)
-if [[ ! -z "$ZURG_WARP_ENABLED" ]]; 
-then
-    echo "ZURG_WARP_ENABLED env var set, doing nothing (you are probably using WARP).."
+if [ "$BYPASS_VPN" == "true" ]; then
+    echo "BYPASS_VPN env var set, doing nothing.."
     sleep infinity
 else
-    exec \
-        /gluetun-entrypoint
+    exec /gluetun-entrypoint
 fi
