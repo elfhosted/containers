64a15f14380c126eec8487f354e137428e99dda2
David Young <davidy@funkypenguin.co.nz>
2025-08-18 21:49:40 +1200

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/nuvio-proxy/ci/latest.sh
+++ apps/nuvio-proxy/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 channel=$1
-version=$(curl -sX GET "https://api.github.com/repos//tapframe/Nuvio-Proxy/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET "https://api.github.com/repos/tapframe/Nuvio-Proxy/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 printf "%s" "${version}"
