79292fa50558c320e787da26e8d35e9aadb52f4d
David Young <davidy@funkypenguin.co.nz>
2025-08-20 13:35:52 +1200

    bump youtubio
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/youtubio/ci/latest.sh
+++ apps/youtubio/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/xXCrash2BomberXx/YouTube-Stremio/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET "https://api.github.com/repos/xXCrash2BomberXx/YouTubio/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 printf "%s" "${version}"
