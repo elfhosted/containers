4dc397a3d86cc053037e2ef0f8dfa915025ade3b
David Young <davidy@funkypenguin.co.nz>
2025-09-04 11:08:16 +1200

    add plex_token to build processes
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- .github/scripts/upstream.sh
+++ .github/scripts/upstream.sh
@@ -4,6 +4,7 @@ APP="${1}"
 CHANNEL="${2}"
 export TOKEN="${TOKEN}"
 export ZURG_GH_CREDS="${ZURG_GH_CREDS}"
+export PLEX_TOKEN="${PLEX_TOKEN}"
 
 if test -f "./apps/${APP}/ci/latest.sh"; then
     bash ./apps/"${APP}"/ci/latest.sh "${CHANNEL}"
