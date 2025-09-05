9965ac7dfae1e3e8bcfddea15828241cee874c2f
David Young <davidy@funkypenguin.co.nz>
2025-09-05 21:05:03 +1200

    update autoscan
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/autoscan/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# If there's no config.yml presented, then add a basic one so that goss can pass tests
-if [[ ! -f "/config/config.yml" ]]; then
-    echo "port: 3030" > /config/config.yml
-fi
-
-exec \
-    /app/autoscan/autoscan
