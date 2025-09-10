23005c8f34740ebcaea256efc8108c8b13609c89
David Young <davidy@funkypenguin.co.nz>
2025-09-11 11:12:59 +1200

    try 2 new images
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/mytrakt/ci/latest.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+channel=$1
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/DemFaR/stremio-trakt-addon/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/DemFaR/stremio-trakt-addon/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
+printf "%s" "${version}"   
---
+++ apps/playlistio/ci/latest.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+channel=$1
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/xXCrash2BomberXx/YouTubio/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/xXCrash2BomberXx/YouTubio/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
+printf "%s" "${version}"   
