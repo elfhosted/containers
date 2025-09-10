b7f07770e03c02948623a96f4c0e05c7ead8aa80
David Young <davidy@funkypenguin.co.nz>
2025-09-11 11:30:50 +1200

    try again
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/youtubio/ci/latest.sh
+++ apps/youtubio/ci/latest.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 channel=$1
 
-LATEST_YOUTUBIO=$(curl -sX GET https://api.github.com/repos/xXCrash2BomberXx/YouTubio/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-
-yt-dlp/yt-dlp
+LATEST_YOUTUBIO=$(curl -sX GET https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 
 if [[ "${channel}" == "dev" ]]; then
     version=$(curl -sX GET "https://api.github.com/repos/xXCrash2BomberXx/YouTubio/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
