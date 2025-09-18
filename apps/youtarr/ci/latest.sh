aa7f76372954370291ab2de77fb366503c80b827
David Young <davidy@funkypenguin.co.nz>
2025-09-18 19:47:24 +1200

    upadte
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/youtarr/ci/latest.sh
+++ apps/youtarr/ci/latest.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-channel=$1
-
 version=$(curl -sX GET https://api.github.com/repos/DialmasterOrg/Youtarr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}
