f3c426933f57eb8b563d84567d473d6676ab04ab
David Young <davidy@funkypenguin.co.nz>
2025-10-02 19:16:40 +1300

    update
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/mytrakt/ci/latest.sh
+++ apps/mytrakt/ci/latest.sh
@@ -2,7 +2,7 @@
 
 channel=$1
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/DemFaR/stremio-trakt-addon/commits/main" --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/DemFaR/stremio-trakt-addon/commits/preview/MyTrakt-debrid-test" --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/DemFaR/stremio-trakt-addon/releases/latest --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | .tag_name')
 fi
