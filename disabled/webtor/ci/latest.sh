1254aae054161ce1abf51073ea70f0e9926d89ae
David Young <davidy@funkypenguin.co.nz>
2025-10-05 11:24:04 +1300

    Add ABBD
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ disabled/webtor/ci/latest.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET https://api.github.com/repos/webtor-io/self-hosted//releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+printf "%s" "${version}"
