8cb9d0e93d595077e04d7329675cb8932a80f8f1
David Young <davidy@funkypenguin.co.nz>
2025-10-12 17:51:11 +1300

    latest build
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/aiometadata/ci/latest.sh
+++ apps/aiometadata/ci/latest.sh
@@ -2,9 +2,8 @@
 
 channel=$1
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/cedya77/aiometadata/tags" --header "Authorization: Bearer ${TOKEN}" jq -r '.[].name | select(test("beta"))' | head -n 1)
+    version=$(curl -sX GET "https://api.github.com/repos/cedya77/aiometadata/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
-    version=$(curl -sX GET https://api.github.com/repos/cedya77/aiometadata/tags --header "Authorization: Bearer ${TOKEN}" | jq -r jq -r '.[].name | select((test("beta") | not) and (test("^v5") | not))' | head -n 1)
-    version="v1.0.0" # hard code for now
+    version=$(curl -sX GET https://api.github.com/repos/cedya77/aiometadata/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
 printf "%s" "${version}"   
