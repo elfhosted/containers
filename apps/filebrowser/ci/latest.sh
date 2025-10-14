05b8ed34fd880a74cc35200df046b77fecfa8554
David Young <davidy@funkypenguin.co.nz>
2025-10-15 12:49:15 +1300

    update

--- apps/filebrowser/ci/latest.sh
+++ apps/filebrowser/ci/latest.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# version=$(curl -sX GET https://api.github.com/repos/filebrowser/filebrowser/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-# version="${version#*v}"
-# version="${version#*release-}"
-# printf "%s" "${version}"
-printf "2.23.0"
+channel=$1
+if [[ "${channel}" == "quantum" ]]; then
+    version=$(curl -sX GET https://api.github.com/repos/gtsteffaniak/filebrowser/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+else
+    printf "2.23.0"
+fi
+printf "%s" "${version}"   
