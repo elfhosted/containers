7eabdaacc93eed6c1e5257f635ce3905f75517f9
David Young <davidy@funkypenguin.co.nz>
2025-11-01 00:30:12 +1300

    fix autostream image
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/autostream/ci/latest.sh
+++ apps/autostream/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET "https://api.github.com/repos/keypop3750/AutoStream/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version=$(curl -sX GET "https://api.github.com/repos/keypop3750/AutoStream/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"
