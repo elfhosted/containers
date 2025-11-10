3bf3f4f2ca8f803883da86be052e74ea922a45fb
David Young <davidy@funkypenguin.co.nz>
2025-11-10 13:30:39 +1300

    add nzbdav dev image

--- apps/nzbdav/ci/latest.sh
+++ apps/nzbdav/ci/latest.sh
@@ -3,7 +3,7 @@
 channel=$1
 
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/snzbdav-dev/nzbdav/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/nzbdav-dev/nzbdav/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/nzbdav-dev/nzbdav/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
