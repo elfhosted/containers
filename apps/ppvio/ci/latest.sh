8eabdb2a8831486e1b241e2fe17a775f255df509
David Young <davidy@funkypenguin.co.nz>
2025-11-13 23:23:29 +1300

    add ppvio
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

---
+++ apps/ppvio/ci/latest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+channel=$1
+
+version=$(curl -sX GET https://api.github.com/repos/xXCrash2BomberXx/ppvio/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+printf "%s" "${version}
