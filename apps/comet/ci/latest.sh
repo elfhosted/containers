7676ceebae057a4bfddefc828d94a76c976c2d1c
David Young <davidy@funkypenguin.co.nz>
2025-12-10 09:34:42 +1300

    experiment with readonly replica support
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/comet/ci/latest.sh
+++ apps/comet/ci/latest.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-
-version=$(curl -sX GET "https://api.github.com/repos/g0ldyy/comet/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-printf "%s" "${version}"
+channel=$1
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/elfhosted/comet/commits/add-readonly-replica-support" --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '.sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/g0ldyy/comet/releases/latest --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | .tag_name')
+fi
+printf "%s" "${version}"   
