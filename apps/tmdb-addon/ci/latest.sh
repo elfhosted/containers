0ff905f4db5fd879ac6bbfc459cec51df0b90cdd
David Young <davidy@funkypenguin.co.nz>
2025-12-10 17:44:28 +1300

    try deploying latest
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/tmdb-addon/ci/latest.sh
+++ apps/tmdb-addon/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/elfhosted/tmdb-addon/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET "https://api.github.com/repos/mrcanelas/tmdb-addon/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 printf "%s" "${version}"
