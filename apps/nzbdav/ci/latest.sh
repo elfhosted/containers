e537ed442f631ad031bbf940c10557894ead14e2
David Young <davidy@funkypenguin.co.nz>
2025-12-20 17:05:42 +1300

    updat
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/nzbdav/ci/latest.sh
+++ apps/nzbdav/ci/latest.sh
@@ -4,8 +4,8 @@ channel=$1
 
 if [[ "${channel}" == "dev" ]]; then
     version=$(curl -sX GET "https://api.github.com/repos/nzbdav-dev/nzbdav/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-elif [[ "${channel}" == "compression" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/elfhosted/nzbdav/commits/feature/sqlite-maintenance" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+elif [[ "${channel}" == "nzb-external-storage" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/elfhosted/nzbdav/commits/feature/nzb-external-storage" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/nzbdav-dev/nzbdav/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
