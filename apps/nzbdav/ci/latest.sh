7fdc4c312a73c37bea841f738dc6eab4c4a69b6b
David Young <davidy@funkypenguin.co.nz>
2025-12-20 20:13:54 +1300

    upadte
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/nzbdav/ci/latest.sh
+++ apps/nzbdav/ci/latest.sh
@@ -4,7 +4,7 @@ channel=$1
 
 if [[ "${channel}" == "dev" ]]; then
     version=$(curl -sX GET "https://api.github.com/repos/nzbdav-dev/nzbdav/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-elif [[ "${channel}" == "nzb-external-storage" ]]; then
+elif [[ "${channel}" == "catalog-storage-offload" ]]; then
     version=$(curl -sX GET "https://api.github.com/repos/elfhosted/nzbdav/commits/feature/nzb-external-storage" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/nzbdav-dev/nzbdav/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
