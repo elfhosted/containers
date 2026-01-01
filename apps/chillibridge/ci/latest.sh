a66c64965a1f016a4e47f4bb9a69971308e8dfbe
David Young <davidy@funkypenguin.co.nz>
2026-01-01 13:03:21 +1300

    update source path
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/chillibridge/ci/latest.sh
+++ apps/chillibridge/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/ikeapod/chilllink-bridge/releases/latest --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | .tag_name')
+version=$(curl -sX GET https://api.github.com/repos/elfhosted/chilllink-bridge/releases/latest --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"
