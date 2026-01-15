e45b260e575d31e98212a395f140b47859de633d
Elfbot <elfbot@elfhosted.com>
2026-01-16 09:23:59 +1300

    update cwadownlaoder (now shelfmark)
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/cwa-downloader/ci/latest.sh
+++ apps/cwa-downloader/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/calibrain/calibre-web-automated-book-downloader/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version=$(curl -sX GET https://api.github.com/repos/calibrain/shelfmark/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"
