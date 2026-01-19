c025e192d02016a8500a8094ea273e5f4aa60118
Elfbot <elfbot@elfhosted.com>
2026-01-20 07:32:33 +1300

    add helm-controller
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

---
+++ apps/helm-controller/ci/latest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET https://api.github.com/repos/fluxcd/helm-controller/releases | jq -r '.[].tag_name' | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n1)
+version="${version#*v}"
+printf "%s" "${version}"
