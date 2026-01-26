a2f0a9ae7618174c14ea0ea18310f51787489356
Elfbot <elfbot@elfhosted.com>
2026-01-26 15:50:58 +1300

    use correct comet repo
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/comet/ci/latest.sh
+++ apps/comet/ci/latest.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 channel=$1
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/elfhosted/comet/commits/main" --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/g0ldyy/comet/commits/main" --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '.sha')
 if [[ "${channel}" == "cometnet" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/g0ldy/comet/commits/feat/cometnet" --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '.sha')
+    version=$(curl -sX GET "https://api.github.com/repos/g0ldyy/comet/commits/feat/cometnet" --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/g0ldyy/comet/releases/latest --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | .tag_name')
 fi
