3d7a7edcc6618454a5208584a5ced79af181cb73
Elfbot <elfbot@elfhosted.com>
2026-02-02 10:48:18 +1300

    update dev
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/strmr/ci/latest.sh
+++ apps/strmr/ci/latest.sh
@@ -2,7 +2,7 @@
 
 channel=$1
 if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET https://api.github.com/repos/godver3/strmr/releases --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.[0].tag_name')
+    version=$(curl -sX GET "https://api.github.com/repos/godver3/strmr/commits?per_page=1" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.[0].sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/godver3/strmr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
