209e7866691131372c4568d8093b857f8c3d8efa
Elfbot <elfbot@elfhosted.com>
2026-02-03 11:11:41 +1300

    try building pulsarr-dev
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/pulsarr/ci/latest.sh
+++ apps/pulsarr/ci/latest.sh
@@ -2,7 +2,7 @@
 
 channel=$1
 if [[ "${channel}" == "develop" ]]; then
-    version=$(curl -sX GET https://api.github.com/repos/jamcalli/pulsarr/commits/master --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+    version=$(curl -sX GET https://api.github.com/repos/jamcalli/pulsarr/commits/develop --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version=$(curl -sX GET https://api.github.com/repos/jamcalli/pulsarr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
