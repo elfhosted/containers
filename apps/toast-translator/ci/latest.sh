a8e4dca894ea9ed77ea6b2e8de0b8adddfef3dc3
Elfbot <elfbot@elfhosted.com>
2026-02-05 06:48:33 +1300

    update toast
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/toast-translator/ci/latest.sh
+++ apps/toast-translator/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 channel=$1
-version=$(curl -sX GET "https://api.github.com/repos/qwertyuiop8899/toast-translator/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+version=$(curl -sX GET https://api.github.com/repos/qwertyuiop8899/toast-translator/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"   
