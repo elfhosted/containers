8d779cf3d16c3b14d254d168cce499b8ff907ac8
Elfbot <elfbot@elfhosted.com>
2026-02-12 09:16:17 +1300

    try fixing cwa
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/calibre-web-automated/ci/latest.sh
+++ apps/calibre-web-automated/ci/latest.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 version="$(curl -sX GET "https://api.github.com/repos/crocodilestick/Calibre-Web-Automated/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
-version="${version#*V}"
 printf "%s" "${version}"
