352fc64bfc963094295b5b5aafe722587dc0fc46
Elfbot <elfbot@elfhosted.com>
2026-02-12 09:49:02 +1300

    minor fix for compatibility on macos
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- .github/scripts/fetch.sh
+++ .github/scripts/fetch.sh
@@ -10,14 +10,13 @@
 #    {"app"...
 # ]
 
-shopt -s lastpipe
+trap 'exit 130' INT
 
-declare -A app_channel_array
 declare -a latest_records=()
-find ./apps -name metadata.json | while read -r metadata; do
-    declare -a __channels=()
+declare -a changes_array=()
+while read -r metadata; do
     app="$(jq --raw-output '.app' "${metadata}")"
-    jq --raw-output -c '.channels | .[]' "${metadata}" | while read -r channels; do
+    while read -r channels; do
         channel="$(jq --raw-output '.name' <<< "${channels}")"
         stable="$(jq --raw-output '.stable' <<< "${channels}")"
         published_version=$(./.github/scripts/published.sh "${app}" "${channel}" "${stable}")
@@ -26,25 +25,13 @@ find ./apps -name metadata.json | while read -r metadata; do
         latest_records+=("$(jo app="${app}" channel="${channel}" stable="${stable}" publishedVersion="${published_version}")")
         if [[ "${published_version}" != "${upstream_version}" ]]; then
             echo "${app}$([[ ! ${stable} == false ]] || echo "-${channel}"):${published_version:-<NOTFOUND>} -> ${upstream_version}"
-            __channels+=("${channel}")
+            changes_array+=("$(jo app="${app}" channel="${channel}")")
         fi
-    done
-    if [[ "${#__channels[@]}" -gt 0 ]]; then
-        app_channel_array[$app]="${__channels[*]}"
-    fi
-done
+    done < <(jq --raw-output -c '.channels | .[]' "${metadata}")
+done < <(find ./apps -name metadata.json)
 
 output="[]"
-if [[ "${#app_channel_array[@]}" -gt 0 ]]; then
-    declare -a changes_array=()
-    for app in "${!app_channel_array[@]}"; do
-        #shellcheck disable=SC2086
-        if [[ -n "${app}" ]]; then
-            for channel in ${app_channel_array[$app]}; do
-                changes_array+=("$(jo app="$app" channel="$channel")")
-            done
-        fi
-    done
+if [[ "${#changes_array[@]}" -gt 0 ]]; then
     #shellcheck disable=SC2048,SC2086
     output="$(jo -a ${changes_array[*]})"
 fi
