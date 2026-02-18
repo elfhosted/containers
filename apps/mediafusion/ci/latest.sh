9f763d92a6b2d9697b6480573bdcb9de3a405645
Elfbot <elfbot@elfhosted.com>
2026-02-19 08:55:26 +1300

    update mediafusion build
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/mediafusion/ci/latest.sh
+++ apps/mediafusion/ci/latest.sh
@@ -5,11 +5,10 @@ if [ "$channel" == "main" ]; then
     # For main channel, get the latest stable release
     version=$(curl -sX GET "https://api.github.com/repos/mhdzumair/MediaFusion/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')
 elif [ "$channel" == "dev" ]; then
-    # For develop channel, get the latest release (including pre-releases)
-    latest_release=$(curl -sX GET "https://api.github.com/repos/mhdzumair/MediaFusion/releases" --header "Authorization: Bearer ${TOKEN}" | jq '.[0]')
-    
-    # Use the tag_name of the latest release, whether it's a pre-release or stable
-    version=$(echo "$latest_release" | jq --raw-output '.tag_name')
+    # For dev channel, get the most recently created release (stable or pre-release)
+    version=$(curl -sX GET "https://api.github.com/repos/mhdzumair/MediaFusion/releases?per_page=100" \
+        --header "Authorization: Bearer ${TOKEN}" | \
+        jq -r '[.[] | select(.draft == false)] | sort_by(.created_at) | last | .tag_name')
 else
     echo "Invalid channel specified"
     exit 1
