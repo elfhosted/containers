dbdcb302559d893c74b59f4678f446ac4c4a6e4c
Elfbot <elfbot@elfhosted.com>
2026-02-20 09:32:25 +1300

    add shelfarr
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

---
+++ apps/shelfarr/ci/latest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+version=$(curl -sX GET https://api.github.com/repos/Pedro-Revez-Silva/shelfarr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+printf "%s" "${version}"   
