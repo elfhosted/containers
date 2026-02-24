51ba2dc7aa3312560e07e5ff4ad60ff9ea706307
Elfbot <elfbot@elfhosted.com>
2026-02-24 15:53:41 +1300

    bulid newtarr
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/huntarr/ci/latest.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/plexguide/Huntarr.io/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-version="${version#*release-}"
-printf "%s" "${version}"
---
+++ apps/newtarr/ci/latest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET https://api.github.com/repos/elfhosted/newtarr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version="${version#*release-}"
+printf "%s" "${version}"
