377cafb714290cbcfaa1fb950d050d71698dad91
Elfbot <elfbot@elfhosted.com>
2026-02-28 13:19:04 +1300

    add mediastorm
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

---
+++ apps/mediastorm/ci/latest.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+channel=$1
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/godver3/mediastorm/commits?per_page=1" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.[0].sha')
+else
+    version=$(curl -sX GET https://api.github.com/repos/godver3/mediastorm/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+fi
+printf "%s" "${version}"
