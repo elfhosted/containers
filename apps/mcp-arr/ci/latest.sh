fb253ec945500d752216e082768588416d1db1cf
Elfbot <elfbot@elfhosted.com>
2026-03-14 23:25:06 +1300

    remove mcp-arr
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/mcp-arr/ci/latest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-channel=$1
-if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/aplaceforallmystuff/mcp-arr/commits?per_page=1" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.[0].sha')
-else
-    version=$(curl -sX GET https://api.github.com/repos/aplaceforallmystuff/mcp-arr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-fi
-printf "%s" "${version}"
