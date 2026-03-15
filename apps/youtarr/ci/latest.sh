1d46cc41b9164c232210f09e4ebda86af13e9409
Elfbot <elfbot@elfhosted.com>
2026-03-16 11:12:32 +1300

    update for ytdlp
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/youtarr/ci/latest.sh
+++ apps/youtarr/ci/latest.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 channel=$1
 
-CHROMEDRIVER_VERSION=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json \
-  | jq -r '.channels.Stable.version')
+LATEST_YTDLP=$(curl -sX GET https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 
 if [[ "${channel}" == "dev" ]]; then
     version=$(curl -sX GET "https://api.github.com/repos/DialmasterOrg/Youtarr/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
 else
     version="$(curl -sX GET "https://api.github.com/repos/DialmasterOrg/Youtarr/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
 fi
-printf "%s" "${version}-${CHROMEDRIVER_VERSION}"
+printf "%s" "${version}-${LATEST_YTDLP}"   
