f2842ee3886d90060d259dc7d06e7c2b78a944e4
Elfbot <elfbot@elfhosted.com>
2026-03-16 10:47:05 +1300

    add chromedriver version to build
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/youtarr/ci/latest.sh
+++ apps/youtarr/ci/latest.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/DialmasterOrg/Youtarr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-printf "%s" "${version}"
+channel=$1
+
+CHROMEDRIVER_VERSION=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json \
+  | jq -r '.channels.Stable.version')
+
+if [[ "${channel}" == "dev" ]]; then
+    version=$(curl -sX GET "https://api.github.com/repos/DialmasterOrg/Youtarr/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
+else
+    version="$(curl -sX GET "https://api.github.com/repos/DialmasterOrg/Youtarr/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
+fi
+printf "%s" "${version}-${CHROMEDRIVER_VERSION}"
