bdb5206ca74301aa35d3541b7da3c5c7ec4bc034
Elfbot <elfbot@elfhosted.com>
2026-03-16 13:00:13 +1300

    update filebrowser
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/filebrowser/ci/latest.sh
+++ apps/filebrowser/ci/latest.sh
@@ -5,5 +5,5 @@ if [[ "${channel}" == "quantum" ]]; then
 else
     version=$(curl -sX GET https://api.github.com/repos/filebrowser/filebrowser/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 fi
-version="${version#-stable}"
+version="${version%-stable}"
 printf "%s" "${version}"
