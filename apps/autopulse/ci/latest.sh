1f14b2f056df0219c17dff1d6af53aa5e729b099
Elfbot <elfbot@elfhosted.com>
2026-03-18 12:44:25 +1300

    add forked autopulse
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/autopulse/ci/latest.sh
+++ apps/autopulse/ci/latest.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/dan-online/autopulse/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+channel=$1
+
+if [[ "${channel}" == "ui" ]]; then
+  version=$(curl -sX GET https://api.github.com/repos/dan-online/autopulse/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+else
+  version=$(curl -sX GET "https://api.github.com/repos/d3v1l1989/targeted-scans/commits/main" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha[0:7]')
+fi
 printf "%s" "${version}"
