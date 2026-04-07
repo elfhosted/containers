cd9b93d0e2d0ac4557dc8268b22695770aa8848b
Elfbot <elfbot@elfhosted.com>
2026-04-07 16:02:40 +1200

    update
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/webstreamr/ci/latest.sh
+++ apps/webstreamr/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/webstreamr/webstreamr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
+version=$(curl -sX GET "https://gitlab.com/api/v4/projects/webstreamr%2Fwebstreamr/releases" | jq --raw-output '.[0].tag_name')
 printf "%s" "${version}"
