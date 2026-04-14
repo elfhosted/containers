970ffde3f3b8e0cbce3b000cd5d340ec49c1180f
Elfbot <elfbot@elfhosted.com>
2026-04-14 15:52:41 +1200

    update
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- apps/wger/ci/latest.sh
+++ apps/wger/ci/latest.sh
@@ -2,5 +2,5 @@
 
 version=$(curl -sX GET https://api.github.com/repos/wger-project/wger/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 
-# printf "%s" "${version}"
-printf %s "2.4"
+version="${version#*v}"
+printf "%s" "${version}"
