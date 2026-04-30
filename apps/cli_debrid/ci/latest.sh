bb741ab5e7abdbec20e759c142e7b7231a0f04ad
David Young <davidy@funkypenguin.co.nz>
2026-04-30 12:20:51 +1200

    fix unwanted apps
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/cli_debrid/ci/latest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-channel=$1
-if [[ "${channel}" == "dev" ]]; then
-    version=$(curl -sX GET https://api.github.com/repos/godver3/cli_debrid/releases --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.[0].tag_name')
-else
-    version=$(curl -sX GET https://api.github.com/repos/godver3/cli_debrid/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-fi
-printf "%s" "${version}"
