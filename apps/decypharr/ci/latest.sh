bfdca42d0e106d8d72a098cf66f9fdcab6d34b97
David Young <davidy@funkypenguin.co.nz>
2026-05-10 19:43:23 +1200

    tidy up
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- apps/decypharr/ci/latest.sh
+++ apps/decypharr/ci/latest.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-channel=$1
-
-if [[ "${channel}" == "beta" ]]; then
-    version=$(curl -sX GET "https://api.github.com/repos/sirrobot01/decypharr/commits/beta" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.sha')
-else
-    version=$(curl -sX GET https://api.github.com/repos/sirrobot01/decypharr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
-fi
+version=$(curl -sX GET https://api.github.com/repos/sirrobot01/decypharr/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 printf "%s" "${version}"
